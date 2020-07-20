5d9fe752a8872ca77740e2e245dc772160fcec80
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-07-20 17:02:37 +0200

    Version 8.2.1

--- main.js
+++ main.js
@@ -264,7 +264,7 @@ const guild = client.guilds.cache.get('703937875720273972')
 
 const updateMembers = (guild) => {
     const channel = guild.channels.cache.get(channelId)
-    channel.setName(`Member count: ${guild.memberCount}`)
+    channel.setName(`Member Count: ${guild.memberCount}`)
 }
 
 client.on('guildMemberAdd', (member) => updateMembers(member.guild))
