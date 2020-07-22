a557b721ca71bbbeecd3c663af71ca813c122c4d
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-07-22 12:51:09 +0200

    Version 2.0.5

--- commands/suggest.js
+++ commands/suggest.js
@@ -2,7 +2,6 @@ module.exports = {
     name: 'suggest',
     description: "suggest command",
     execute(message, args){
-        message.delete(1000);
         message.reply('a link to the Study Vibes calendar can be found here: https://tinyurl.com/yawqzozw');
     }
 }
