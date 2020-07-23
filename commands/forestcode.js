c4ddca03539a4438f3e62950991ea53746c35760
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-07-23 08:25:53 +0200

    Version 2.0.1

---
+++ commands/forestcode.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'forestcode',
+    description: "!code command that gives forest embed",
+    execute(message, args){
+        const channel = client.channels.cache.get('703937876634894388');
+        const exampleEmbed = new Discord.MessageEmbed()
+            .setColor('#337f4e')
+            .setTitle(`${message.author.username} wants to plant a tree! :evergreen_tree:`)
+            .setThumbnail('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1.bp.blogspot.com%2F-E77muf7rE5k%2FUD5FaQznK8I%2FAAAAAAAAIh4%2FGJXlKvLB7WM%2Fs1600%2FTree%2BIn%2BField%2BWallpapers%2B3.jpg&f=1&nofb=1')
+            .setTimestamp()
+            .setFooter(`Planter: ${message.author.username} `)
+            .addFields(
+                { name: '\u200B', value: 'Forest info:' },
+                { name: `Use code: ${args[0]} or click this link: https://www.forestapp.cc/join-room?token=${args[0]}.`, value: '\u200B' },
+                { name: ':stopwatch: Duration:', value:`${args[1]}` , inline: true},
+                { name: ':closed_lock_with_key: Starting in:', value: `${args[2]}`, inline: true},
+                { name: '\u200B', value: 'Good luck! :palm_tree:' }
+                )
+
+        channel.send(exampleEmbed);
+        const welcome = client.channels.cache.get('732292791287283862');
+        welcome.send(exampleEmbed);
+        }    
+}
--- main.js
+++ main.js
@@ -35,6 +35,10 @@ client.on('message', message =>{
         client.commands.get('cal').execute(message, args);
     };
 
+    if(command === 'code'){
+        client.commands.get('forestcode').execute(message, args);
+    };
+
     if (message.content.startsWith(`${pref}help`)) {
         message.channel.send('Help categories include: **?cmd**, **?forest**, **?VC**, **?hydra**, **?focus**, **?team**, **?birthday**, **?timers**, **?todo**. Please type either one to get more info!.');
     } else if (message.content.startsWith(`${pref}forest`)){
@@ -147,35 +151,6 @@ client.on('message', message =>{
         Use: **?help**, **?cmd**, **?forest**, **?VC**, **?hydra**, **?focus**, **?team**, **?birthday**, **?timers**, **?todo** for more specific info!`);
             }
 
-});
-
-//Forest code input section
-client.on('message', message =>{
-    if(!message.content.startsWith(prefic) || message.author.bot) return;
- 
-    const args = message.content.slice(prefic.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-    
-    if(command === 'code'){
-        const channel = client.channels.cache.get('703937876634894388');
-        const exampleEmbed = new Discord.MessageEmbed()
-            .setColor('#337f4e')
-            .setTitle(`${message.author.username} wants to plant a tree! :evergreen_tree:`)
-            .setThumbnail('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1.bp.blogspot.com%2F-E77muf7rE5k%2FUD5FaQznK8I%2FAAAAAAAAIh4%2FGJXlKvLB7WM%2Fs1600%2FTree%2BIn%2BField%2BWallpapers%2B3.jpg&f=1&nofb=1')
-            .setTimestamp()
-            .setFooter(`Planter: ${message.author.username} `)
-            .addFields(
-                { name: '\u200B', value: 'Forest info:' },
-                { name: `Use code: ${args[0]} or click this link: https://www.forestapp.cc/join-room?token=${args[0]}.`, value: '\u200B' },
-                { name: ':stopwatch: Duration:', value:`${args[1]}` , inline: true},
-                { name: ':closed_lock_with_key: Starting in:', value: `${args[2]}`, inline: true},
-                { name: '\u200B', value: 'Good luck! :palm_tree:' }
-                )
-
-        channel.send(exampleEmbed);
-        const welcome = client.channels.cache.get('732292791287283862');
-        welcome.send(exampleEmbed);
-        }
 });
 
 //Forest automatic link section
