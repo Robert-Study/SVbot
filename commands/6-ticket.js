d66c3f04bab306d6fe20f927ae1165c4c6b5210d
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-07-24 07:29:30 +0200

    Version 3.0.1

--- commands/6-ticket.js
+++ commands/6-ticket.js
@@ -5,6 +5,6 @@ module.exports = {
         message.reply(', your ticket has been submitted, a Mod will PM you shortly.');
         const modchannel = message.client.channels.cache.get('707525743575302144');
         let modrole = "707581746937462794";
-        modchannel.send(`<@${message.author.id}> has a problem, please <@${modrole}> PM this member.`);
+        modchannel.send(`<@${message.author.id}> has a problem, please <@&${modrole}> PM this member.`);
     }
 }
