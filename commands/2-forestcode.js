28b56cd3b185624da8dea446f9e9af0496645547
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-07-25 14:36:34 +0200

    Version 3.5.0

--- commands/2-forestcode.js
+++ commands/2-forestcode.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, args){
         const Discord = require('discord.js');
         const channel = message.client.channels.cache.get('703937876634894388');
-        const exampleEmbed = new Discord.MessageEmbed()
+        let exampleEmbed = new Discord.MessageEmbed()
             .setColor('#337f4e')
             .setTitle(`${message.author.username} wants to plant a tree! :evergreen_tree:`)
             .setThumbnail('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1.bp.blogspot.com%2F-E77muf7rE5k%2FUD5FaQznK8I%2FAAAAAAAAIh4%2FGJXlKvLB7WM%2Fs1600%2FTree%2BIn%2BField%2BWallpapers%2B3.jpg&f=1&nofb=1')
@@ -18,8 +18,13 @@ module.exports = {
                 { name: '\u200B', value: 'Good luck! :palm_tree:' }
                 )
 
-        channel.send(exampleEmbed);
+        let channelembed = await channel.send(exampleEmbed);
+        channelembed.react(':busts_in_silhouette:')
+        channelembed.react(':🔇:')
+
         const welcome = message.client.channels.cache.get('732292791287283862');
-        welcome.send(exampleEmbed);
+        let welcomeembed = await welcome.send(exampleEmbed);
+        welcomeembed.react(':busts_in_silhouette:')
+        welcomeembed.react(':🔇:')
         }    
 }
