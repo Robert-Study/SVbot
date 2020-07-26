072359e22688e282c946b87dfba14de0705a147c
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-07-26 10:16:21 +0200

    Version 6.0.6

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+
+const { Client } = require('discord.js');
+const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
 const prefix = "Plant trees with me on Forest! Enter "
 const prefic = "!"
@@ -77,6 +79,16 @@ client.on('message', async message =>{
         welcomeembed.react('🔇')
         }    
     
+});
+
+client.on("messageReactionAdd", async (reaction, user) => {
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch
+
+    if (user.bot) return;
+    if (reaction.emoji.name === '👥'){
+        await reaction.message.guild.members.cache.get(user.id).roles.add("")
+    }
 });
 
 
