620ae305a1e5782963c636317663b2d874392745
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-07-26 10:26:03 +0200

    Version 6.0.7

--- main.js
+++ main.js
@@ -91,6 +91,16 @@ client.on("messageReactionAdd", async (reaction, user) => {
     }
 });
 
+client.on("messageReactionAdd", async (reaction, user) => {
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch
+
+    if (user.bot) return;
+    if (reaction.emoji.name === '🔇'){
+        await reaction.message.guild.members.cache.get(user.id).roles.add("729706682308886548")
+        await reaction.message.guild.members.cache.get(user.id).roles.remove("707547622591692911")
+    }
+});
 
 //Prefic = '?' commands:
 client.on('message', message =>{
