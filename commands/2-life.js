7be06bb03ee076d17c27d1b265b8985788a994b2
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-07-29 17:20:41 +0200

    Version 7.2.1

--- commands/2-life.js
+++ commands/2-life.js
@@ -8,7 +8,7 @@ module.exports = {
         .addFields(
             { name: 'What is life', value: ` 
             Life is indeed difficult, partly because of the real difficulties we must overcome in order to survive, and partly because of our own innate desire to always do better, to overcome new challenges, to self-actualize. Happiness is experienced largely in striving towards a goal, not in having attained things, because our nature is always to want to go on to the next endeavor.
-            [info][add][mail][note]Albert Ellis, Michael Abrams, Lidia Dengelegi, The Art & Science of Rational Eating, 1992` }
+            *Albert Ellis, Michael Abrams, Lidia Dengelegi, The Art & Science of Rational Eating, 1992*` }
             )
     message.channel.send(focusembed);
         }
