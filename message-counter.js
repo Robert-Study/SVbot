0d04d0f0eebd039c42a74459819f9213ad1aa921
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-07-30 16:40:14 +0200

    Version 1.0.1

--- message-counter.js
+++ message-counter.js
@@ -28,32 +28,4 @@ module.exports = (client) => {
       }
     })
   })
-}
-
-module.exports.getmessageCount = async (UserId) => {
-    return await mongo().then(async (mongoose) => {
-      try {
-        console.log('Running findOne()')
-  
-        const result = await messageCountSchema.findOne({
-          UserId,
-        })
-  
-        let messageCount = 0
-
-        if (result) {
-          messageCount = result.messageCount
-        } else {
-          console.log('Inserting a document')
-          await new messageCountSchema({
-            UserId,
-            messageCount,
-          }).save()
-        }
-  
-        return messageCount
-      } finally {
-        mongoose.connection.close()
-      }
-    })
-  }
+};
