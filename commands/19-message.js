192cb11c5ee82028dc57e7701c07db942ea4c7f6
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-02 02:06:00 +0200

    Version 2.0.1

--- commands/19-message.js
+++ commands/19-message.js
@@ -1,13 +1,13 @@
 module.exports = {
     name: '1-cal',
     description: "calendar command",
-    execute (message,args) {
+    execute = async (message,args) => {
         const messagecounter = require('../../getmessagecount')
         const target = message.mentions.users.first() || message.author
         const targetId = target.id
 
         const UserId = target.id
 
-        const messages = async() => { await messagecounter.getmessageCount(UserId)}
+        let messages = await messagecounter.getmessageCount(UserId)
         message.send(`${target} has already written ${messages} messages on this server!`)}
     }
