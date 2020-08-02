f4c265fcca04a03df5fdd08626c1155af58dc79f
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-03 01:27:45 +0200

    Version 4

--- commands/19-message.js
+++ commands/19-message.js
@@ -1,12 +1,10 @@
-module.exports.getmessage = async (message, args) => {
-                const messagecounter = require('./getmessagecount')
-                const target = message.mentions.users.first() || message.author
-                const targetId = target.id
+const messagecounter = require('./getmessagecount')
+const target = message.mentions.users.first() || message.author
+const UserId = target.id
 
-                const UserId = target.id
-
-                let messages = await messagecounter.getmessageCount(UserId)
-                message.reply(`you have already written ${messages} messages on this server!`)
+module.exports.getmessage = async (UserId) => {
+        let messages = await messagecounter.getmessageCount(UserId)
+        message.reply(`you have already written ${messages} messages on this server!`)
 }
 
 
