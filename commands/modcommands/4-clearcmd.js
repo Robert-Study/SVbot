720cb80363c0363d2b5040939f4a72cba0134d1d
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-03 23:49:21 +0200

    Version 2.0.1

--- commands/modcommands/4-clearcmd.js
+++ commands/modcommands/4-clearcmd.js
@@ -5,13 +5,10 @@ module.exports = {
     permissions: 'BAN_MEMBERS',
     callback: async (message, arguments, text) => {
         const amount = arguments[0]
-        if (isNaN(amount)) return 
-            message.reply('The amount parameter isn`t a number!'); 
+        if (isNaN(amount)) return message.reply('The amount parameter isn`t a number!'); 
 
-        if (amount > 100) return 
-            message.reply('You can`t delete more than 100 messages at once!'); 
-        if (amount < 1) return 
-            message.reply('You have to delete at least 1 message!'); 
+        if (amount > 100) return message.reply('You can`t delete more than 100 messages at once!'); 
+        if (amount < 1)  return message.reply('You have to delete at least 1 message!'); 
 
         await message.channel.messages.fetch({ limit: amount }).then(messages => { 
             message.channel.bulkDelete(messages)}
