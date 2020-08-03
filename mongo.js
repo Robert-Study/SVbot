c2ec783f91c94288746c0f3d6af3671f7004af93
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-03 20:29:05 +0200

    Version 2.0.1

---
+++ mongo.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const mongoPath = `mongodb+srv://DatabaseUser:9q2HgkPYsIvwL6mL@cluster0.s12v0.mongodb.net/first-db?retryWrites=true&w=majority`
+
+module.exports = async () => {
+  await mongoose.connect(mongoPath, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+
+  return mongoose
+}
