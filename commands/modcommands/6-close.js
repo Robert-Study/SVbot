17e1037faafda243d8a95082f2e0dfde49882dde
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-07 08:11:02 +0200

    Version 2.1.0

--- commands/modcommands/6-close.js
+++ commands/modcommands/6-close.js
@@ -1,13 +1,16 @@
 module.exports = {
     commands: ['close'],
-    minArgs: 0,
-    maxArgs: 0,
+    minArgs: 1,
+    maxArgs: 1,
     permissions: 'BAN_MEMBERS',
 
     callback: (message, arguments, text) => {
         const categoryId = "703937876634894387";
+        var person = message.guild.member(message.mentions.users.first());        
         if (message.channel.parentID == categoryId) {
             message.channel.delete();
+            const logchannel = message.guild.channels.cache.get('730029372697870347');
+            logchannel.send(`The support channel of ${"<@" + person.user.id + ">"} has been closed by ${"<@" + message.author.id + ">"}`)
  
     } else {
         message.channel.send("This command only works in a support channel");
