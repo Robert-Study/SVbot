a265bffb98aa244af3a0af02989c1f1e93424b76
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-07 11:00:00 +0200

    Version 2.0.1

--- commands/helpcommands/11-helpticket.js
+++ commands/helpcommands/11-helpticket.js
@@ -3,6 +3,6 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     callback: (message, arguments, text) => {
-        message.channel.send('Use **!ticket** to open up a __personal__ support channel on this server. The support channel can be used to, ask general questions, report bugs or problems with the server, report a user that is not behaving or that is being innapropriate to you or a fellow user on the server or in a PM. **Please also use the !ticket support channels to report when you are stuck in the Focus Lock.** It is the easiest way for a mod to recognize and help you out!');
+        message.channel.send('Use **!ticket** to open up a __personal__ support channel. The support channel can be used to, ask general questions, report bugs or problems with the server, report a user that is not behaving or that is being innapropriate to you or a fellow user on the server or in a PM. **Please also use the !ticket support channels to report when you are stuck in the Focus Lock.** It is the easiest way for a mod to recognize and help you out!');
     }
 }
