df524740cef7e531728ed64bbcea9231d6119d20
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-07 11:55:18 +0200

    Version 2.1.0

--- commands/usercommands/2-start.js
+++ commands/usercommands/2-start.js
@@ -4,7 +4,7 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     callback: (message, arguments, text) => {
-        message.reply(`You are now part of the *@Study Team* and will be pinged with every Team reminder!`);
+        message.reply(`You are now part of the **Study Team** and will be pinged with every Team reminder!`);
         message.member.roles.add('729444698812579870');
    }
 }
--- commands/usercommands/3-focus.js
+++ commands/usercommands/3-focus.js
@@ -6,7 +6,7 @@ module.exports = {
     callback: (message, arguments, text) => {
         message.reply(`went into deep Focus Mode - don't disturb!`);
         const focus = message.client.channels.cache.get('730185814822223962');
-        focus.send(`Welcome in **Focus Mode** <@${message.author.id}> - to exit please use **!end**.`);
+        focus.send(`Welcome in **Focus** mode <@${message.author.id}> - to exit please use **!end**.`);
         message.member.roles.add('729706682308886548');
         message.member.roles.remove('707547622591692911');
     }
