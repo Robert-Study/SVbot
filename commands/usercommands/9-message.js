3fd11b8080df198b202875173ee1a7cc3c7aa9e4
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-08 10:42:40 +0200

    Version 2.1.0

--- commands/usercommands/9-message.js
+++ commands/usercommands/9-message.js
@@ -4,6 +4,6 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     callback: (message, arguments, text) => {
-        message.channel.send('**Because there are so many problems with the !lock these days I have opened unlock open to anyone if needed. Use !lock @/YOUR OWN USERNAME.**' );
+        message.channel.send('**Because there are so many problems with the !lock these days I have opened unlock open to anyone if needed. Use !unlock @/YOUR OWN USERNAME.**' );
     }
 }
