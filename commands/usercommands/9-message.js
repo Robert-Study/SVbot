86c709b3adefa579b07c268df86be1354fb00244
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-08 10:36:34 +0200

    Version 2.1.0

---
+++ commands/usercommands/9-message.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    commands: ['ann'],
+    minArgs: 0,
+    maxArgs: 0,
+    callback: (message, arguments, text) => {
+        message.reply('**Because there are so many problems with the !lock these days I have opened unlock open to anyone if needed. Use !lock @/YOUR OWN USERNAME.**' );
+    }
+}
