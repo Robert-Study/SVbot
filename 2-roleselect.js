cd28616aa1f2dd836115476b9994017a7177aa53
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-12 23:17:33 +0200

    Version 2.1.0

--- 2-roleselect.js
+++ 2-roleselect.js
@@ -27,9 +27,7 @@ client.on("messageReactionAdd", async (reaction, user) => {
     if (reaction.message.partial) await reaction.message.fetch();
     if (reaction.partial) await reaction.fetch
 
-    const welcomechannel = reaction.message.guild.channel === "707532591514910731"
-
-    if (user.bot || !welcomechannel) return;
+    if (user.bot) return;
     if (reaction.emoji.name === '✅'){
         await reaction.message.guild.members.cache.get(user.id).roles.add("707547622591692911")
         await reaction.message.guild.members.cache.get(user.id).roles.remove("739920051208978495")
