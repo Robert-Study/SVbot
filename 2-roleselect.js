1c4e69be6341c4d5671b59d35a9d0c3124fd71a1
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-13 11:36:58 +0200

    Version 2.1.0

--- 2-roleselect.js
+++ 2-roleselect.js
@@ -17,10 +17,11 @@ client.on("messageReactionAdd", async (reaction, user) => {
     if (reaction.partial) await reaction.fetch
 
     if (user.bot) return;
+    if (reaction.message.channel.id !== "732631130133495889"){
     if (reaction.emoji.name === '🔇'){
         await reaction.message.guild.members.cache.get(user.id).roles.add("729706682308886548")
         await reaction.message.guild.members.cache.get(user.id).roles.remove("707547622591692911")
-    }
+    }}
 });
 
 client.on("messageReactionAdd", async (reaction, user) => {
@@ -54,5 +55,25 @@ client.on("messageReactionAdd", async (reaction, user) => {
             await reaction.message.guild.members.cache.get(user.id).roles.add("732327573899575338")
     }}
 });
+
+client.on("messageReactionRemove", async (reaction, user) => {
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch
+
+    if (user.bot) return;
+    if (reaction.message.channel.id === "732631130133495889"){
+    if (reaction.emoji.name === '🔇'){
+        await reaction.message.guild.members.cache.get(user.id).roles.remove("709519094524936245")
+        }else if (reaction.emoji.name === '🤖'){
+            await reaction.message.guild.members.cache.get(user.id).roles.remove("717822162244993064")
+        }else if (reaction.emoji.name === '📚'){
+            await reaction.message.guild.members.cache.get(user.id).roles.remove("717675345918165014")
+        }else if (reaction.emoji.name === '📖'){
+            await reaction.message.guild.members.cache.get(user.id).roles.remove("717675473152245800")
+        }else if (reaction.emoji.name === '👨‍🏫'){
+            await reaction.message.guild.members.cache.get(user.id).roles.remove("732327573899575338")
+    }}
+});
+
 
 }
