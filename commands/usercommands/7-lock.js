ade01b9c9b36e08da44b3779462fdba1b4775c4e
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-15 09:24:19 +0200

    Version 2.1.0

--- commands/usercommands/7-lock.js
+++ commands/usercommands/7-lock.js
@@ -23,15 +23,15 @@ module.exports = {
                     message.member.roles.add(focusrole.id);
                     message.member.roles.remove(verifiedrole.id);
                     const focus = message.guild.channels.cache.get('730185814822223962');
+                    const general = message.guild.channels.cache.get('703937876634894388');
                     focus.send(`${"<@" + message.author.id + ">"}, you have now been **Locked** in Focus for ${ms(ms(time))}`)
-                    message.send(`Getting you locked up in there, good luck!`)
+                    general.send(`Getting you locked up in there, good luck!`)
 
                     setTimeout(function(){
                         message.member.roles.remove(role.id);
                         message.member.roles.remove(focusrole.id);
                         message.member.roles.add(verifiedrole.id);
                         console.log(role.id)
-                        const general = message.guild.channels.cache.get('703937876634894388');
                         general.send(`${"<@" + message.author.id + ">"}, you have now been unlocked`)
                     }, ms(time));
                 }
