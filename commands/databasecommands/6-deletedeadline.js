e6cb88573d0fe8e1aa34ed74e21bdccbf92ede3e
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-30 20:41:40 +0200

    Version 2.1.0

---
+++ commands/databasecommands/6-deletedeadline.js
@@ -0,0 +1,25 @@
+module.exports = {
+    commands: ['deletedl', 'deletedeadlines'],
+    minArgs: 0,
+    maxArgs: 0,
+    callback: async(message, arguments, text) => {
+        const mongo = require('../../mongo')
+        const deadlineSchema = require('../../schemas/3-deadlineschema')
+        const target = message.mentions.users.first() || message.author
+        const UserID = target.id
+
+    await mongo().then(async (mongoose) => {
+            try {
+            console.log('Connected to mongodb!')
+
+            await deadlineSchema.deleteOne({
+            UserID,
+            })
+    } finally {
+      mongoose.connection.close()
+    }
+})
+}
+}
+
+
