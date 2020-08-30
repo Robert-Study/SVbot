0e4933476d8462190cbc561226a200e264eae7d8
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-30 13:29:11 +0200

    Version 2.1.0

--- commands/usercommands/9-submission.js
+++ commands/usercommands/9-submission.js
@@ -1,10 +1,12 @@
 module.exports = {
     commands: ['sub', 'submission'],
     minArgs: 1,
+    expectedArgs : '<!sub #app> or <!sub #tip>',
 
     callback: (message, arguments, text) => {
         let channel = arguments[0]
-        if(channel === 'app'){
+        if(channel === '#app'){
+            message.delete()
             const Discord = require('discord.js');
             let appchannel = message.guild.channels.cache.get('708032923428716626');
             let apptext = text
@@ -17,7 +19,23 @@ module.exports = {
                 { name: 'App recommendation:', value: `${apptext}` })
         
             appchannel.send(AppEmbed);}
-        else appchannel.send('Not found')
+        
+        else if(channel === '#tip'){
+            message.delete()
+            const Discord = require('discord.js');
+            let tipchannel = message.guild.channels.cache.get('708651085765804093');
+            let tiptext = text
+            let AppEmbed = new Discord.MessageEmbed()
+                .setColor('#337f4e')
+                .setTitle(`${message.author.username} has a studytip:`)
+                .setTimestamp()
+                .setFooter(`Recommended by: ${message.author.username} `)
+                .addFields(
+                    { name: 'Study tip:', value: `${tiptext}` })
+            
+            tipchannel.send(AppEmbed);}
+        
+        else message.send('Please use the tags when making submissions!')
     }  
 }
 
