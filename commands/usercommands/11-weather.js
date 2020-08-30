8f4cd401e6cc9c58327deeb72c73d8bacba66520
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-30 23:03:06 +0200

    Version 2.2.0

---
+++ commands/usercommands/11-weather.js
@@ -0,0 +1,28 @@
+module.exports = {
+    commands: ['weather', 'wt'],
+    minArgs: 1,
+    expectedArgs : '<!weather location>',
+
+    callback: async(message, arguments, text) => {
+        weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
+            try {
+             
+            let embed = new discord.MessageEmbed()
+            .setTitle(`Weather - ${result[0].location.name}`)
+            .setColor("#ff2050")
+            .setDescription("Temperature units can may be differ some time")
+            .addField("Temperature", `${result[0].current.temperature} Celcius`, true)
+            .addField("Sky Text", result[0].current.skytext, true)
+            .addField("Humidity", result[0].current.humidity, true)
+            .addField("Wind Speed", result[0].current.windspeed, true)
+            .addField("Observation Time", result[0].current.observationtime, true)
+            .addField("Wind Display", result[0].current.winddisplay, true)
+            .setThumbnail(result[0].current.imageUrl);
+               message.channel.send(embed)
+            } catch(err) {
+              return message.channel.send("Unable To Get the data of Given location")
+            }
+            });   
+                
+              }
+            }
