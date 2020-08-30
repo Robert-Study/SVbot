cfb33558eeebf8cfd8d4f7de4752d7bb6e5e97a0
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-30 14:48:01 +0200

    Version 2.1.0

---
+++ commands/usercommands/10-deadline.js
@@ -0,0 +1,23 @@
+module.exports = {
+    commands: ['dl', 'deadline'],
+    minArgs: 1,
+    expectedArgs : '<!dl 28/08/2020 Exam on...>',
+
+    callback: async(message, arguments, text) => {
+        let date = arguments[0]
+        let deadlinechannel = message.guild.channels.cache.get('717829409679343628');
+        const Discord = require('discord.js');
+        let dltext = text
+            let dlEmbed = new Discord.MessageEmbed()
+            .setColor('#337f4e')
+            .setTitle(`${message.author.username} Added deadline:`)
+            .setTimestamp()
+            .setFooter(`Deadline by: ${message.author.username} `)
+            .addFields(
+                { name: `${date}`, value: `${dltext}` })
+            
+            let reactapp = await deadlinechannel.send(dlEmbed);
+                reactapp.react('👍')
+                reactapp.react('🍀')
+            }
+}
