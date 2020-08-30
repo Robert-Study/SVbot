7b3710d04bcfb5cc4d0551327fbe972d148e2973
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-30 16:03:10 +0200

    Version 2.1.0

---
+++ commands/helpcommands/13-helpdeadline.js
@@ -0,0 +1,18 @@
+module.exports = {
+    commands: ['helpdeadline', 'helpdl', 'infodl', 'infodeadline'],
+    minArgs: 0,
+    maxArgs: 0,
+    callback: (message, arguments, text) => {
+        const Discord = require('discord.js');
+        const vcembed = new Discord.MessageEmbed()
+            .setColor('#337f4e')
+            .addFields(
+                { name: 'Deadline and exams submission', value: 
+                `How to register you deadline or exam date:
+                
+                Register your deadlines with the !dl command, followed by the date and the exam/deadline you are working towards 
+                (**Example:** !dl 01/09/2020 Super secret project).` }
+            )
+    message.channel.send(vcembed);
+        }
+}
