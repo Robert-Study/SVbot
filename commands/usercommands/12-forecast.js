60f992bf5102937a8d129240c5cbf99c8c0fb717
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-31 00:35:57 +0200

    Version 2.2.0

--- commands/usercommands/12-forecast.js
+++ commands/usercommands/12-forecast.js
@@ -12,7 +12,7 @@ module.exports = {
                 let forec = result[0].forecast
                 let reply = ''
                 for (const forecast of forec) {
-                    reply += `**${forecast.shortday}** it will be *${forecast.high} C∘* and *${forecast.skytextday} with ${forecast.precip}% rain. \n\n`
+                    reply += `**${forecast.shortday}** it will be *${forecast.high} C∘* and **${forecast.skytextday}** with ${forecast.precip}% rain. \n\n`
                 }
                 console.log(`${reply}`)
             let embed = new discord.MessageEmbed()
