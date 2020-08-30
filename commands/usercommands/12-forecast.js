76f597417fb6cb9f0a019385f285eca997b5b9b0
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-31 00:16:14 +0200

    Version 2.2.0

---
+++ commands/usercommands/12-forecast.js
@@ -0,0 +1,28 @@
+module.exports = {
+    commands: ['forecast', 'fc'],
+    minArgs: 1,
+    expectedArgs : '<!forecast location>',
+
+    callback: async(message, arguments, text) => {
+        const weather = require('weather-js');
+        const discord = require('discord.js');
+        weather.find({search: arguments.join(" "), degreeType: 'C'}, function(err, result) {
+            try {
+                let reply = ''
+                for (const forecast of result[0]) {
+                    reply += `**${forecast.day}** named *${forecast.high}*\n\n`
+                }
+            let embed = new discord.MessageEmbed()
+            .setTitle(`Forecast - ${result[0].location.name}`)
+            .setColor("#ff2050")
+            .setDescription("Temperature units can may be differ some time")
+            .addField("Temperature", `${reply}`, true)
+            
+            message.channel.send(embed)
+            } catch(err) {
+              return message.channel.send("Unable To Get the data of Given location")
+            }
+            });   
+                
+              }
+            }
