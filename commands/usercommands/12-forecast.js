0195db135850796d92ab3862d6b4264f47f28ba9
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-08-31 00:23:55 +0200

    Version 2.2.0

--- commands/usercommands/12-forecast.js
+++ commands/usercommands/12-forecast.js
@@ -9,8 +9,9 @@ module.exports = {
         weather.find({search: arguments.join(" "), degreeType: 'C'}, function(err, result) {
             try {
                 console.log(`${result[0].location.name}`)
-                let reply = ''
-                for (const forecast of result[0]) {
+                let forec = result[0]
+                let reply = 'D'
+                for (const forecast of forec) {
                     reply += `**${forecast.day}** named *${forecast.high}*\n\n`
                 }
                 console.log(`${reply}`)
