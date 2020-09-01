c745ae70a92944bb77586e44668b2ed533cd1583
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-01 10:04:35 +0200

    Version 2.2.0

--- commands/usercommands/13-anon.js
+++ commands/usercommands/13-anon.js
@@ -2,10 +2,10 @@ module.exports = {
     commands: ['anon', 'anonymous'],
     minArgs: 1,
     expectedArgs : '<!anon text>',
-    permissions: 'BAN_MEMBERS',
 
     callback: async(message, arguments, text) => {
         message.delete()
-        message.channel.send(text);
+        const general = message.client.channels.cache.get('750264645662539816');
+        general.send(text);
     }
 }
