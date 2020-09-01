78fed0256a598198c7af4e663757fb694d47fdd2
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-01 23:47:06 +0200

    Version 2.2.0

--- commands/usercommands/14-health.js
+++ commands/usercommands/14-health.js
@@ -3,6 +3,6 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     callback: (message, arguments, text) => {
-        message.channel.send('Hi! Are you looking for some **(mental) health advice**? Please consider these Discord Servers with professional help, \n\n **Nutrition and Health** https://discord.gg/Jpz5KN7 \n\n**Mental Health** https://discord.gg/HHAwFyY , https://discord.gg/WfrMsyG ');
+        message.channel.send('Hi! Are you looking for some **(mental) health advice**? Please consider these Discord Servers with professional help, \n\n**Nutrition and Health** https://discord.gg/Jpz5KN7 \n\n**Mental Health** https://discord.gg/HHAwFyY , https://discord.gg/WfrMsyG ');
     }
 }
