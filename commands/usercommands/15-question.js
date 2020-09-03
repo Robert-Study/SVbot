262b65703dd48347a38e3b71152b7b7fe48f908b
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-03 10:42:03 +0200

    Version 2.2.0

--- commands/usercommands/15-question.js
+++ commands/usercommands/15-question.js
@@ -1,7 +1,7 @@
 module.exports = {
     commands: ['question', 'life', 'icebreaker'],
     minArgs: 0,
-    maxArgs: 0,
+    maxArgs: 1,
     callback: (message, arguments, text) => {
         const { randomQuestion } = require('../../randomQuestion.json');
         const Discord = require('discord.js');
