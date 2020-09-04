7c8bd076edf0447d8cc500e15f7553819819f64a
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-04 20:35:19 +0200

    Version 2.3.0

--- 10-countchannel.js
+++ 10-countchannel.js
@@ -1,10 +1,11 @@
 module.exports = (client) => {
     client.on('message', (message) => {
-      if (message.author.bot) return;
-      if (message.channel.id === "751498035699122196"){
-          const number = content.toLowerCase() 
-          if (isNaN(number)) {
-            message.reply('You did not provide a number. Start counting at 1.')}
+        const { member, content, guild } = message
+        if (message.author.bot) return;
+        if (message.channel.id === "751498035699122196"){
+            const number = content.toLowerCase() 
+            if (isNaN(number)) {
+                message.reply('You did not provide a number. Start counting at 1.')}
           }
     })
 }
