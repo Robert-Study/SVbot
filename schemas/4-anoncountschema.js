e5edaeab62dfd28642155fcfaf943fcc1d1b1027
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-04 16:26:54 +0200

    Version 2.3.0

--- schemas/4-anoncountschema.js
+++ schemas/4-anoncountschema.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
 const annonCountSchema = mongoose.Schema({
+    UserId: {
+        type: String,
+        required: true,
+      },
+      
   messageCount: {
     type: Number,
     required: true,
