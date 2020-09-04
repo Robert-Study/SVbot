9bae3b8180614537f9a6b99ffb87130cbba7bc9d
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-04 20:33:08 +0200

    Version 2.3.0

--- 10-countchannel.js
+++ 10-countchannel.js
@@ -2,7 +2,7 @@ module.exports = (client) => {
     client.on('message', (message) => {
       if (message.author.bot) return;
       if (message.channel.id === "751498035699122196"){
-          const number = message
+          const number = content.toLowerCase() 
           if (isNaN(number)) {
             message.reply('You did not provide a number. Start counting at 1.')}
           }
