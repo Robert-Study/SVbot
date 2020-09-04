88e22401833253f3f0c589f0c30e935944dbef08
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-04 21:01:57 +0200

    Version 2.3.0

--- 10-countchannel.js
+++ 10-countchannel.js
@@ -1,3 +1,5 @@
+const Countchannelschema = require("./schemas/6-countchannelschema");
+
 module.exports = (client) => {
     client.on('message', (message) => {
         const { member, content, guild } = message
@@ -5,7 +7,54 @@ module.exports = (client) => {
         if (message.channel.id === "751498035699122196"){
             const number = content.toLowerCase() 
             if (isNaN(number)) {
-                message.reply('You did not provide a number. Start counting at 1.')}
+                message.reply('you did not provide a number. Start counting at 1.')}
+            else
+                await mongo().then(async (mongoose) => {
+                    try {
+                    console.log('Searching the database for message-count')
+            
+                  const result = await Countchannelschema.findOne({
+                    UserId: 'annon'
+                  })
+            
+                  let messageCount = 0
+          
+                  if (result) {
+                    messageCount = await result.messageCount
+                    if (number != (result) + 1){
+                        message.reply('that is the wrong number, Start counting at 1.')
+                    } else {
+                        console.log('Unexpected error')}
+
+                return messageCount}
+                    }finally {
+                    mongoose.connection.close()
+                }
+                    })
+                
+
+                await mongo().then(async (mongoose) => {
+                    try {
+                      await Countchannelschema
+                        .findOneAndUpdate(
+                            {
+                                UserId: 'annon',
+                              },
+                              {
+                            $inc: {
+                              messageCount: 1,
+                            },
+                          },
+                          {
+                            upsert: true,
+                          }
+                        )
+                        .exec()
+                    } finally {
+                      mongoose.connection.close()
+                    }
+                }
+                )
           }
     })
 }
