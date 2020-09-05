e2ed706dae157deab4ad1cda3cd20454f2d2999f
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-05 03:05:10 +0200

    Version 2.3.0

--- 11-countbug.js
+++ 11-countbug.js
@@ -1,44 +1,22 @@
-module.exports = (client) => {
-    client.on('message', async (message) => {
-        const { member, content, guild } = message
-        if (message.author.bot) return;
-        if (message.channel.id === "730029372697870347"){
-            console.log("channel detected")
-            const number = content.toLowerCase() 
-            const fs = require('fs');
-            const counter = require('./counter.json')
+client.on('message', ({channel, content, member}) => {
+    let count = 0
+    let timeout
 
-            let rawdata = fs.readFileSync('./counter.json');
-            let data = JSON.parse(rawdata);
-            if ((data.result.messagecount +1) != number ){
-                message.reply('that is the wrong number, I will start with 1, you can start counting at 2.')
-                data.result.splice(1,1);
-                let result = {
-                    name : "counter",
-                    messagecount : 1
-                }
-                let newdata = JSON.stringify(result, null, 2);
-                fs.writeFile('student-3.json', newdata, (err) => {
-                    if (err) throw err;
-                console.log('Data written to file');
-                console.log(`${newdata}`)
-                });
-            console.log('This is after the write call');
-            }
-            else { 
-                let correctnumber = (data.result.messagecount +1)
-                let correctresult = {
-                    name : "counter",
-                    messagecount : correctnumber
-                }
-                data.result.splice(1,1)
-                let newcorrect = JSON.stringify(correctresult, null, 2);
-                fs.writeFile('counter.json', correctresult, (err) => {
-                    if (err) throw err;
-                    console.log('something went wrong');
-                });
-            console.log(`New number correctly added (${newcorrect})`)
-            }
+    if (channel.id === '730029372697870347') {
+      if (member.user.bot) return
+      if (Number(content) === count + 1) {
+        count++
+        if (timeout) client.clearTimeout(timeout)
+        timeout = client.setTimeout(
+          () => channel.send(++count).catch(console.error),
+          3000000
+        )
 
-        }}
-    )}
+      } else if (member.id !== client.user.id) {
+        channel.send(`${member} messed up! Start counting from 1!`).catch(console.error)
+        count = 0
+        if (timeout) client.clearTimeout(timeout)
+      }
+    }
+  })
+  
