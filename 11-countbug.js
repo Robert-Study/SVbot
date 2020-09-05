3f1812b6173157264796b94bab9c12d75359c5c5
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-05 03:33:50 +0200

    Version 2.3.0

--- 11-countbug.js
+++ 11-countbug.js
@@ -3,28 +3,17 @@ module.exports = (client) => {
         const { member, content, guild } = message
         let number = content.toLowerCase() 
         let count = 0
-        let timeout
 
         if (message.channel.id === '730029372697870347') {
             console.log('channel found')
             console.log(number)
             if (member.user.bot) return
-            if (number === (count +1)) {
-                console.log('adding 1')
+            if (number !== (count +1)) {
+                message.channel.send(`${member} messed up! Start counting from 1!`).catch(console.error)
+                count = 0}
+            else{ 
                 count++
-                console.log(count)
-            if (timeout) client.clearTimeout(timeout)
-                timeout = client.setTimeout(
-                () => channel.send(++count).catch(console.error),
-                3000000
-        )
-
-      } else if (member.id !== client.user.id) {
-        message.channel.send(`${member} messed up! Start counting from 1!`).catch(console.error)
-        count = 0
-        console.log(count)
-        if (timeout) client.clearTimeout(timeout)
-      }
-    }
-  })
+            }
+        }
+    })
 }
