1cfd7783219ffb5388fa35d977df4e9195e0cf37
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-07 12:26:52 +0200

    Version 2.3.0

---
+++ commands/modcommands/7-getsuggest.js
@@ -0,0 +1,37 @@
+module.exports = {
+    commands: ['showsuggestions', 'showsuggest', 'suggestionlist', 'suggestions'],
+    minArgs: 1,
+    maxArgs: 1,
+    permissions: 'BAN_MEMBERS',
+
+    callback: (message, arguments, text) => {
+        const mongo = require('../../mongo')
+        const suggestdataSchema = require('../../schemas/6-suggestdataschema')
+        UserId = "annon"
+
+        return await mongo().then(async (mongoose) => {
+            try {
+              console.log('Searching the database for Suggestions')
+        
+              const results = await suggestdataSchema.find({
+                UserId,
+              })
+
+              const sortedsuggestions = Object.keys(results).sort(
+                (a, b) => suggestcount[b] - suggestcount[a])
+
+                console.log(sortedsuggestions)
+                sortedsuggestions.length = 5
+
+              let reply = 'Here you go: \n\n'
+                for (const result of sortedsuggestions) {
+                    reply += `**${result.suggestcount}** deadline: *${result.suggestion}*\n\n`
+                }
+
+    
+} finally {
+    mongoose.connection.close()}
+
+})
+}
+}
