e77f486bae16b6ab83affe39b4439ef2c9100985
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-10 15:35:22 +0200

    Version 2.4.0

--- commands/modcommands/9-event.js
+++ commands/modcommands/9-event.js
@@ -9,7 +9,7 @@ module.exports = {
         const hydraembed = new Discord.MessageEmbed()
         .setColor('#1a9cd8')
         .addFields(
-            { name: 'Event Notifications', value: `It has been brought to our attention that using colours to vote might not be the best way as people suffering from colorblindness might have difficulties voting. We appologize for this and will use other emojis for polls in the feature! \n
+            { name: 'Event Notifications', value: `It has been brought to our attention that using colours to vote might not be the best way as people suffering from colorblindness might have difficulties voting. We appologize for this and will use other emojis for polls in the future! \n
             If you want to stay informed of the events and polls in this channel, please click the 🔔 reaction below!`}
             )
     message.channel.send(hydraembed); 
