46baa4a8878d3720ad19f22ed94fc40c725c8c2e
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-11 17:43:19 +0200

    Version 2.4.0

--- commands/usercommands/20-todoadd.js
+++ commands/usercommands/20-todoadd.js
@@ -39,36 +39,38 @@ module.exports = {
             let messageCount = 0
 
             if (result) {
-                messageCount = await result.messageCount
-                const todochannel = message.client.channels.cache.get('729372656456958022');
-                let newtodo = text
-                let uptodo = {
-                    UserId: UserId,
-                    todocount: messageCount,
-                    todo: newtodo
+                try {
+                    messageCount = await result.messageCount
+                    const todochannel = message.client.channels.cache.get('729372656456958022');
+                    let newtodo = text
+                    let uptodo = {
+                        UserId: UserId,
+                        todocount: messageCount,
+                        todo: newtodo
+                    }
+                    await new tododataSchema(uptodo).save(function (err, doc) {
+                        if (err) return console.log(err);
+                        console.log("Document-todo inserted succussfully!");
+                    })
+
+                } finally {
+                    const results = await tododataSchema.find({
+                        UserId,
+                    })
+                    console.log(results)
+
+                    let sortmyresults = results
+                    console.log(sortmyresults)
+                    let reply = '**Your To-do List:** \n\n'
+                    for (const newresult of sortmyresults) {
+                        reply += `**${newresult.todocount})** *${newresult.todo}*\n`
+                    }
+                    message.channel.send(reply)
+
+
+                    mongoose.connection.close()
+
                 }
-                await new tododataSchema(uptodo).save(function (err, doc) {
-                    if (err) return console.log(err);
-                    console.log("Document-todo inserted succussfully!");
-                })
-
-
-                const results = await tododataSchema.find({
-                    UserId,
-                })
-                console.log(results)
-
-                let sortmyresults = results
-                console.log(sortmyresults)
-                let reply = '**Your To-do List:** \n\n'
-                for (const newresult of sortmyresults) {
-                    reply += `**${newresult.todocount})** *${newresult.todo}*\n`
-                }
-                message.channel.send(reply)
-
-
-                mongoose.connection.close()
-
             }
         })
     }
