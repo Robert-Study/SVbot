cf5996e203b2620117c81b0e6d7fb6dd049a02d0
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-12 05:56:30 +0200

    Version 3.2.0

--- commands/usercommands/25-setupmorning.js
+++ commands/usercommands/25-setupmorning.js
@@ -10,44 +10,40 @@ module.exports = {
         const UserId = mention.id
         let providedplace = arguments[0]
         let providedquote = arguments[1]
-        let providedforecast = arguments[2] 
+        let providedforecast = arguments[2]
 
         console.log(providedforecast)
         console.log(providedquote)
 
-        if (providedquote != 'yes' || providedquote != 'no') {
-            message.reply('please provide a yes or no answer for Quotes')
-        }
-        else if ((providedforecast != 'yes') || (providedforecast != 'no')) {
-            message.reply('please provide a yes or no answer for forecast-info')
-        }
-        else {
-            await mongo().then(async (mongoose) => {
-                try {
-                    console.log('Running findOneAndUpdate()')
+        if (providedquote == 'yes' || providedquote == 'no') {
+            if ((providedforecast == 'yes') || (providedforecast == 'no')) {
+                await mongo().then(async (mongoose) => {
+                    try {
+                        console.log('Running findOneAndUpdate()')
 
-                    const result = await userdataSchema.findOneAndUpdate(
-                        {
-                            UserId,
-                        },
-                        {
-                            place: providedplace,
-                            setQuote: providedquote,
-                            setForecast: providedforecast
-                        },
+                        const result = await userdataSchema.findOneAndUpdate(
+                            {
+                                UserId,
+                            },
+                            {
+                                place: providedplace,
+                                setQuote: providedquote,
+                                setForecast: providedforecast
+                            },
 
-                        {
-                            upsert: true,
-                            new: true,
-                        }
-                    )
-                } finally {
-                    mongoose.connection.close()
-                    message.reply(', your morning setup has been arranged.')
-                }
-            })
+                            {
+                                upsert: true,
+                                new: true,
+                            }
+                        )
+                    } finally {
+                        mongoose.connection.close()
+                        message.reply(', your morning setup has been arranged.')
+                    }
+                })
 
-        }
+            }else message.reply('You have not provided correct input for providedforecast')
+        }else message.reply('You have not provided correct input for providedquote')
     }
 }
 
