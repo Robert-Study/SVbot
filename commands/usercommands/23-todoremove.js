ff54e646046bfe4368906d15442ff2239364f73b
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-13 12:05:17 +0200

    Version 3.6.1

--- commands/usercommands/23-todoremove.js
+++ commands/usercommands/23-todoremove.js
@@ -16,48 +16,49 @@ module.exports = {
 
 
         await mongo().then(async (mongoose) => {
-            await tododataSchema.deleteOne({
-                UserId,
-                todocount: removecount,
-            })
-            message.reply(`Deleted task #${removecount}. To add new tasks use !todoadd`)
-            let newtodo = await tododataSchema.find({
-                UserId,
-            })
+            try {
+                await tododataSchema.deleteOne({
+                    UserId,
+                    todocount: removecount,
+                })
+                message.reply(`Deleted task #${removecount}. To add new tasks use !todoadd`)
+                let newtodo = await tododataSchema.find({
+                    UserId,
+                })
 
-            newtodo.forEach(async item => {
-                let i = item.todocount;
-                console.log(i)
-                if(i > removecount) {
-                    await tododataSchema.findOneAndUpdate({
-                        UserId,
-                        todocount: i,
-                    }, {
+                newtodo.forEach(async item => {
+                    let i = item.todocount;
+                    console.log(i)
+                    if (i > removecount) {
+                        await tododataSchema.findOneAndUpdate({
+                            UserId,
+                            todocount: i,
+                        }, {
 
-                        $inc: { todocount: -1 }
-
-
-                    })
-                }
-            })
+                            $inc: { todocount: -1 }
 
 
-            await todoCountSchema
-                .findOneAndUpdate(
-                    {
-                        UserId: UserId,
-                    },
-                    {
-                        $inc: {
-                            messageCount: -1,
-                        },
-                    },
-                    {
-                        upsert: true,
+                        })
                     }
-                )
-                .exec()
-            mongoose.connection.close()
+                })
+
+
+                await todoCountSchema
+                    .findOneAndUpdate(
+                        {
+                            UserId: UserId,
+                        },
+                        {
+                            $inc: {
+                                messageCount: -1,
+                            },
+                        },
+                        {
+                            upsert: true,
+                        }
+                    )
+                    .exec()
+            } finally { mongoose.connection.close() }
 
         })
 
