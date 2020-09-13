6fc49ab657a6b197a536f21c1da34f5d277479c3
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-13 14:41:36 +0200

    Version 4.0.1

--- commands/modcommands/9-event.js
+++ commands/modcommands/9-event.js
@@ -9,8 +9,12 @@ module.exports = {
         const hydraembed = new Discord.MessageEmbed()
         .setColor('#1a9cd8')
         .addFields(
-            { name: 'Event Notifications', value: `It has been brought to our attention that using colours to vote might not be the best way as people suffering from colorblindness might have difficulties voting. We appologize for this and will use other emojis for polls in the future! \n
-            If you want to stay informed of the events and polls in this channel, please click the 🔔 reaction below!`}
+            { name: 'To-Do page update', value: `To all users of the to-do page, we have implemented the functions of the todo-bot of this page into our own SV-bot. To transition to the SV bot, we kindly ask you to use the following commands when setting up a to-do list: \n
+            **!todo** : Shows your to-do list, will be empty for all new-users\n
+            **!todoadd 'task' : Will add a task to your to-do list\n
+            **!todoremove 'number' : Will remove the task with the number from the list\n
+            **!todoclear : Will delete the entire list. \n\n
+            The old To-do bot will get disabled once we have the feeling that most users have moved to the SV bot. If you need help, contact our team!`}
             )
     message.channel.send(hydraembed); 
     }
