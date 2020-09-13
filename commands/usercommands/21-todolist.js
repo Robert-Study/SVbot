8278df7997e11d76bbd0567757ea2ceff52c6770
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-13 14:36:19 +0200

    Version 4.1.0

--- commands/usercommands/21-todolist.js
+++ commands/usercommands/21-todolist.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const tododataSchema = require('../../schemas/8-tododataschema')
 
 module.exports = {
-    commands: ['tatodo', 'todolist'],
+    commands: ['todo', 'todolist'],
     minArgs: 0,
     expectedArgs: '<!todo>',
 
