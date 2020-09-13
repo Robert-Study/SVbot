8cf68b447c9aba279ae3f462403fcf671176b4ac
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-13 14:43:17 +0200

    Version 4.0.1

--- commands/modcommands/9-event.js
+++ commands/modcommands/9-event.js
@@ -11,9 +11,9 @@ module.exports = {
         .addFields(
             { name: 'To-Do page update', value: `To all users of the to-do page, we have implemented the functions of the todo-bot of this page into our own SV-bot. To transition to the SV bot, we kindly ask you to use the following commands when setting up a to-do list: \n
             **!todo** : Shows your to-do list, will be empty for all new-users\n
-            **!todoadd 'task' : Will add a task to your to-do list\n
-            **!todoremove 'number' : Will remove the task with the number from the list\n
-            **!todoclear : Will delete the entire list. \n\n
+            **!todoadd 'task'** : Will add a task to your to-do list\n
+            **!todoremove 'number'** : Will remove the task with the number from the list\n
+            **!todoclear** : Will delete the entire list. \n\n
             The old To-do bot will get disabled once we have the feeling that most users have moved to the SV bot. If you need help, contact our team!`}
             )
     message.channel.send(hydraembed); 
