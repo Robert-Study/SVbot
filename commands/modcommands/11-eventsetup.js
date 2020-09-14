77355415576f16705afc91103bfb6d26a256516b
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-14 16:28:11 +0200

    Version 4.2.1

--- commands/modcommands/11-eventsetup.js
+++ commands/modcommands/11-eventsetup.js
@@ -5,20 +5,11 @@ module.exports = {
 
 
     callback: (message, arguments, text) => {
-        message.channel.send('What is the date of your event?').then(() => {
-            message.channel.awaitMessages( { max: 1, time: 30000, errors: ['time'] })
-                .then(collected => { let date = message.content }
-                )
-            if (date) {
-                message.channel.send('What is the time of your event?').then(() => {
-                    message.channel.awaitMessages( { max: 1, time: 30000, errors: ['time'] })
-                        .then(collected => { let time = message.content 
-                            console.log(`${date} and ${time}`)}
+        const filter = m => m.content.includes('discord');
+        const collector = message.channel.createMessageCollector(filter, { time: 15000 });
 
-                        )
-
-                })
-            }
-        })
+        collector.on('collect', m => {
+            console.log(`Collected ${m.content}`);
+        });
     }
 }
