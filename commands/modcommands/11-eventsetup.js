a1c2d9207f7f6e9d80acf0ac40e92f7cbac3ed0b
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-14 16:42:07 +0200

    Version 4.3.1

--- commands/modcommands/11-eventsetup.js
+++ commands/modcommands/11-eventsetup.js
@@ -7,12 +7,14 @@ module.exports = {
     callback: (message, arguments, text) => {
         
         
-        const filter = m => m.content.includes('discord') && m.author.id === message.author.id
+        const filter = m => m.content.includes('!') && m.author.id === message.author.id
 
         message.channel.send("What is your age?").then(() => {
             message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
                 .then(collected => {
+                    const date = message.content
                     message.channel.send(`${collected.first().author} got the correct answer!`);
+                    console.log(date)
                 })
                 .catch(collected => {
                     message.channel.send('Looks like nobody got the answer this time.');
