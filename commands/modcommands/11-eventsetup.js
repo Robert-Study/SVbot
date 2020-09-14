f87a53f12e03b2433728892aa6f6c1870a6f0374
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-14 17:33:42 +0200

    Version 4.3.1

--- commands/modcommands/11-eventsetup.js
+++ commands/modcommands/11-eventsetup.js
@@ -8,16 +8,23 @@ module.exports = {
         const Discord = require('discord.js')
         const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
         message.channel.send("Please write your age.")
-        collector.on('collect', message => {
+        collector.once('collect', message => {
             let age = message.content
             message.channel.send(`${age}`)
             console.log(age)
+            if (age) {
+                message.channel.send("Please write your date.")
+                collector.once('collect', message => { 
+                let date = message.content
+                message.channel.send(`${date}`)
+                console.log(date)
+            })
+        }
         })
     }
 }
 
-        
-        
 
-            
-                
+
+
+
