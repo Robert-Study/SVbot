efc4e3625abf80c473c232404777e13bf45f57ad
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-14 13:51:12 +0200

    Version 4.2.1

--- commands/modcommands/9-event.js
+++ commands/modcommands/9-event.js
@@ -2,20 +2,16 @@ module.exports = {
     commands: ['event'],
     minArgs: 0,
     maxArgs: 0,
-    permissions: 'BAN_MEMBERS',
+   
 
     callback: (message, arguments, text) => {
         const Discord = require('discord.js');
         const hydraembed = new Discord.MessageEmbed()
         .setColor('#1a9cd8')
         .addFields(
-            { name: 'To-Do page update', value: `To all users of the to-do page, we have implemented the functions of the todo-bot of this page into our own SV-bot. To transition to the SV bot, we kindly ask you to use the following commands when setting up a to-do list: \n
-            **!todo** : Shows your to-do list, will be empty for all new-users\n
-            **!todoadd 'task'** : Will add a task to your to-do list\n
-            **!todoremove 'number'** : Will remove the task with the number from the list\n
-            **!todoclear** : Will delete the entire list. \n\n
-            The old To-do bot will get disabled once we have the feeling that most users have moved to the SV bot. If you need help, contact our team!`}
-            )
+            { name: 'Event planned - Saturday 19/Sep 2020', value:'**SD** will give a presentation on the topic "Permaculture." To prepare for his presentation at university. Interested people can join. The presentation will be in a private channel named "Permaculture" \n\n**19 September 2020, Saturday**\nDuration: 20-30 minutes + Q/A\nTime: 10:30 PM IST (Indian Standard Time)\n\nPermaculture is an ethical and sustainable way of doing farming. As explained by one of the founders of this idea Bill Mollison, " The philosophy behind permaculture is one of working with, rather than against nature, of protracted and thoughtful observation rather than protracted and thoughtless action; of looking at systems in all their functions, rather than asking only one yield of them; and of allowing systems to demonstrate their own evolution."\nGiven how industrial agriculture has led to so many ecological and climate concerns, it becomes essential to look for better farming methods. Permaculture has the potential to address all the problems caused by industrial agriculture.\nDisclaimer: I dont have prior experience in giving a presentation, so it may feel boring to some of you. But you may learn some new concepts that are very crucial for addressing social and climate change concerns related to agriculture.'
+        })
+            
     message.channel.send(hydraembed); 
     }
 }
