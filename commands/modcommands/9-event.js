a14c1abf290a0d5abdaf02cc12d76152d55383fb
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-14 23:46:35 +0200

    Version 4.3.4

--- commands/modcommands/9-event.js
+++ commands/modcommands/9-event.js
@@ -24,22 +24,23 @@ module.exports = {
         let providedtime = result.time
         let providedheader = result.header
         let provideddescription = result.description
+        let providedbarcode = result.barcode
+        if (providedbarcode >= 0) {
+            console.log(provideddescription)
 
-        console.log(provideddescription)
-
-        let eventEmbed = new Discord.MessageEmbed()
-            .setColor('#337f4e')
-            .setTitle(`${providedheader}`)
-            .setTimestamp()
-            .setFooter(`Event host: ${message.author.username} `)
-            .addFields(
-                { name: "New event", value: `**${message.author.username}** has planned an event, the event is open to attend for any verified member. A specific channel with the event title will be set up on the event date and time. Please, when attending, make sure you are on the specified time in the channel and **respect your host.** A reminder is set on the date 2 hours prior to the event, to receive a reminder click the ✅ reaction.` },
-                { name: "Date:", value: `${provideddate}`, inline: true },
-                { name: "Time:", value: `${providedtime}`, inline: true },
-                { name: "Event description:", value: `${provideddescription}` },
-            )
-        message.channel.send(eventEmbed)
-
+            let eventEmbed = new Discord.MessageEmbed()
+                .setColor('#337f4e')
+                .setTitle(`${providedheader}`)
+                .setTimestamp()
+                .setFooter(`Event host: ${message.author.username} `)
+                .addFields(
+                    { name: "New event", value: `**${message.author.username}** has planned an event, the event is open to attend for any verified member. A specific channel with the event title will be set up on the event date and time. Please, when attending, make sure you are on the specified time in the channel and **respect your host.** A reminder is set on the date 2 hours prior to the event, to receive a reminder click the ✅ reaction.` },
+                    { name: "Date:", value: `${provideddate}`, inline: true },
+                    { name: "Time:", value: `${providedtime}`, inline: true },
+                    { name: "Event description:", value: `${provideddescription}` },
+                )
+            message.channel.send(eventEmbed)
+        } else { message.reply('no event found!') }
     }
 }
 
