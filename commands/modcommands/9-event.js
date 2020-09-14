e2249b832abed9cd2fc09cab7fd88419f74bf061
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-14 23:38:52 +0200

    Version 4.3.4

--- commands/modcommands/9-event.js
+++ commands/modcommands/9-event.js
@@ -19,28 +19,27 @@ module.exports = {
 
         console.log(`here are the ${result}`)
 
-        for (items of result) {
-            let provideddate = items.date
-            let providedtime = items.time
-            let providedheader = items.header
-            let provideddescription = items.description
-            
-            console.log(provideddescription)
 
-            let eventEmbed = new Discord.MessageEmbed()
-                .setColor('#337f4e')
-                .setTitle(`${providedheader}`)
-                .setTimestamp()
-                .setFooter(`Event host: ${message.author.username} `)
-                .addFields(
-                    { name: "New event", value: `**${message.author.username}** has planned an event, the event is open to attend for any verified member. A specific channel with the event title will be set up on the event date and time. Please, when attending, make sure you are on the specified time in the channel and **respect your host.** A reminder is set on the date 2 hours prior to the event, to receive a reminder click the ✅ reaction.` },
-                    { name: "Date:", value: `${provideddate}`, inline: true },
-                    { name: "Time:", value: `${providedtime}`, inline: true },
-                    { name: "Event description:", value: `${provideddescription}` },
-                )
-            message.channel.send(eventEmbed)
+        let provideddate = result.date
+        let providedtime = result.time
+        let providedheader = result.header
+        let provideddescription = result.description
 
-        }
+        console.log(provideddescription)
+
+        let eventEmbed = new Discord.MessageEmbed()
+            .setColor('#337f4e')
+            .setTitle(`${providedheader}`)
+            .setTimestamp()
+            .setFooter(`Event host: ${message.author.username} `)
+            .addFields(
+                { name: "New event", value: `**${message.author.username}** has planned an event, the event is open to attend for any verified member. A specific channel with the event title will be set up on the event date and time. Please, when attending, make sure you are on the specified time in the channel and **respect your host.** A reminder is set on the date 2 hours prior to the event, to receive a reminder click the ✅ reaction.` },
+                { name: "Date:", value: `${provideddate}`, inline: true },
+                { name: "Time:", value: `${providedtime}`, inline: true },
+                { name: "Event description:", value: `${provideddescription}` },
+            )
+        message.channel.send(eventEmbed)
+
     }
-
 }
+
