2504bf518f8bd0dbcdec56acb14c815a931fff52
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-14 16:23:02 +0200

    Version 4.2.1

--- commands/modcommands/11-eventsetup.js
+++ commands/modcommands/11-eventsetup.js
@@ -6,12 +6,12 @@ module.exports = {
 
     callback: (message, arguments, text) => {
         message.channel.send('What is the date of your event?').then(() => {
-            message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+            message.channel.awaitMessages( { max: 1, time: 30000, errors: ['time'] })
                 .then(collected => { let date = message.content }
                 )
             if (date) {
                 message.channel.send('What is the time of your event?').then(() => {
-                    message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+                    message.channel.awaitMessages( { max: 1, time: 30000, errors: ['time'] })
                         .then(collected => { let time = message.content 
                             console.log(`${date} and ${time}`)}
 
