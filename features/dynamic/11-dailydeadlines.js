97976a9e7ea65944482244baf1b2bb7c92a51bce
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-16 23:42:09 +0200

    Version 5.4.5

--- features/dynamic/11-dailydeadlines.js
+++ features/dynamic/11-dailydeadlines.js
@@ -1,47 +1,49 @@
-module.exports = async(client) => {
-    var schedule = require('node-schedule');
+module.exports = (client) => {
+    client.on('ready', async () => {
+        var schedule = require('node-schedule');
 
-    var j = schedule.scheduleJob('42 * * * *', function () {
-        const Deadlineschema = require("@schemas/3-deadlineschema")
-        var moment = require('moment');
-        const Discord = require('discord.js');
-        const testchannel = guild.channels.cache.get('754042973850828821')
+        var j = schedule.scheduleJob('45 * * * *', function () {
+            const Deadlineschema = require("@schemas/3-deadlineschema")
+            var moment = require('moment');
+            const Discord = require('discord.js');
+            const testchannel = guild.channels.cache.get('754042973850828821')
 
-        let current = moment().format("DD/MM/YYYY")
-        console.log(current)
+            let current = moment().format("DD/MM/YYYY")
+            console.log(current)
 
-        let dlresults = await Deadlineschema.find({
-            date: current
-        })
-        console.log(dlresults)
+            let dlresults = await Deadlineschema.find({
+                date: current
+            })
+            console.log(dlresults)
 
 
-        if (dlresults) {
-            let reply = ''
-            try {
-                for (items of dlresults) {
-                    let User = await message.guild.members.fetch(items.UserID)
-                    let deadline = items.dltext
-                    console.log(deadline)
+            if (dlresults) {
+                let reply = ''
+                try {
+                    for (items of dlresults) {
+                        let User = await message.guild.members.fetch(items.UserID)
+                        let deadline = items.dltext
+                        console.log(deadline)
 
-                    reply += `**${User}** deadline: *${deadline}*\n\n`
+                        reply += `**${User}** deadline: *${deadline}*\n\n`
+                    }
+                } finally {
+                    const todayEmbed = new Discord.MessageEmbed()
+                        .setColor('#337f4e')
+                        .setTitle(`Todays deadlines:`)
+                        .addFields(
+                            { name: 'Deadlines:', value: `${reply}` },
+                        )
+
+                    let reacttoday = await testchannel.send(todayEmbed);
+                    reacttoday.react('🍀')
                 }
-            } finally {
-                const todayEmbed = new Discord.MessageEmbed()
-                    .setColor('#337f4e')
-                    .setTitle(`Todays deadlines:`)
-                    .addFields(
-                        { name: 'Deadlines:', value: `${reply}` },
-                    )
-
-                let reacttoday = await testchannel.send(todayEmbed);
-                reacttoday.react('🍀')
             }
-        }
 
 
 
-        console.log('The answer to life, the universe, and everything!');
+            console.log('The answer to life, the universe, and everything!');
 
-    });
+        });
+    })
 }
