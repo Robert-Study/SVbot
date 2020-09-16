c566b1045571389d1c506802f1a35233e483e6b1
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-16 14:44:53 +0200

    Version 5.4.1

--- commands/usercommands/6-in-development/beta.js
+++ commands/usercommands/6-in-development/beta.js
@@ -1,24 +1,28 @@
 const Deadlineschema = require("@schemas/3-deadlineschema")
+var moment = require('moment');
 
 module.exports = {
     commands: ['betashowdl'],
-    minArgs: 1,
-    maxArgs: 1,
+    minArgs: 0,
+    maxArgs: 0,
 
 
     callback: async (message, arguments, text) => {
-        let update = arguments[0]
+        let current = moment().format("DD/MM/YYY")
+        console.log(current)
+
         let dlresults = await Deadlineschema.find({
-            date: update
+            date: current
         })
         console.log(dlresults)
 
+        let reply = '**Deadlines today:** \n\n'
+
         for (items of dlresults) {
-            let User = items.UserID
+            let User = client.users.cache.get(items.UserID)
             let deadline = items.dltext
             console.log(deadline)
 
-            let reply = 'Deadlines today: \n\n'
             reply += `**${User}** deadline: *${deadline}*\n\n`
 
             message.channel.send(reply)
