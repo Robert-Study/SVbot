9361423715ad653cc6b324cab1aaa3800dd205e0
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-16 14:04:14 +0200

    Version 5.0.1

--- features/dynamic/7-addmessage.js
+++ features/dynamic/7-addmessage.js
@@ -1,5 +1,5 @@
 
-const messageSchema = require('@schemas/1-messagecountschema')
+const messageSchema = require('@schemas/12-messagecountschema')
 const messageCache = {}
 
 module.exports = (client) => { }
