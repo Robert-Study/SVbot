486a02df8cff57bb3b3c57cde040595793184461
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-16 23:48:46 +0200

    Version 5.4.5

--- features/dynamic/11-dailydeadlines.js
+++ features/dynamic/11-dailydeadlines.js
@@ -2,7 +2,7 @@ module.exports = (client) => {
     client.on('ready', async () => {
         var schedule = require('node-schedule');
 
-        var j = schedule.scheduleJob('45 * * * *', async function () {
+        var j = schedule.scheduleJob('52 * * * *', async function () {
             const Deadlineschema = require("@schemas/3-deadlineschema")
             var moment = require('moment');
             const Discord = require('discord.js');
