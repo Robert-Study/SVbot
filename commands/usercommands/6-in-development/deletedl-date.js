74ccddfe700c442072b59dfe81a52c7f77107efc
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-17 02:26:47 +0200

    Version 5.4.5

---
+++ commands/usercommands/6-in-development/deletedl-date.js
@@ -0,0 +1,27 @@
+module.exports = {
+    commands: ['betadelete'],
+    minArgs: 1,
+    maxArgs: 1,
+    callback: async (message, arguments, text) => {
+
+        const deadlineSchema = require('@schemas/3-deadlineschema')
+        const target = message.mentions.users.first() || message.author
+        const UserID = target.id
+        let deleteddate = arguments[0]
+
+        console.log('Connected to mongodb!')
+
+        let todelete = await deadlineSchema.get({
+            UserID,
+            date: deleteddate
+        })
+        if (todelete) {
+            await deadlineSchema.deleteOne({
+                UserID,
+                date: deleteddate
+            })
+
+            message.reply(`I have deleted your deadline on the ${deleteddate}`)
+        }else message.reply('could not find your deadline, please check your command')
+    }
+}
