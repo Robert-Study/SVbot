3014997dadb1e36cbcf55e73f17442af065e69c7
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-17 14:56:09 +0200

    Version 5.4.5

--- features/dynamic/12-birthday.js
+++ features/dynamic/12-birthday.js
@@ -2,7 +2,7 @@ module.exports = (client) => {
     client.on('ready', async () => {
         var schedule = require('node-schedule');
 
-        var j = schedule.scheduleJob('55 * * * *', async function () {
+        var j = schedule.scheduleJob('58 * * * *', async function () {
             const birthdayschema = require("@schemas/13-birthdayschema")
             var moment = require('moment');
             const Discord = require('discord.js');
