1aab0d73e246fb3c99e760633badb71809817f5a
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-18 10:46:59 +0200

    Version 5.4.5

--- commands/usercommands/2-focus-lock-team/5-remaininglock.js
+++ commands/usercommands/2-focus-lock-team/5-remaininglock.js
@@ -25,7 +25,7 @@ module.exports = {
                 console.log(unlocktime)
                 console.log(timeremaining)
 
-                message.reply(`You are still locked until **${unlocktime} GMT-time**  which is **${timeremaining}** from now`)
+                message.reply(`you are still locked until **${unlocktime} GMT-time**  which is approximately **${timeremaining}** from now`)
             }
         }
     }
