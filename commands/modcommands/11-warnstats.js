0f0c642183dd035a75eb767030ad73ee56e9a1ac
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-20 14:19:26 +0200

    Version 5.4.6

---
+++ commands/modcommands/11-warnstats.js
@@ -0,0 +1,36 @@
+module.exports = {
+    commands: ['stat', 'warnstats'],
+    minArgs: 1,
+    maxArgs: 1,
+    permissions: 'BAN_MEMBERS',
+
+    callback: async (message, arguments, text) => {
+        var person = message.guild.member(message.mentions.users.first());
+        if (!person) return message.reply("I CANT FIND THE USER " + person);
+
+        const warningcountSchema = require('@schemas/1-warningcount')
+        const messageCountSchema = require('@schemas/12-messagecount')
+
+
+        warnings = await warningcountSchema.findOne({
+            UserID: author
+        })
+        messages = await messageCountSchema.findOne({
+            Userid: author
+        })
+
+        for (items of warnings) {
+            let count = items.warnings
+            for (counts of messages) {
+                let messagecount = items.messageCount
+
+                let average = (messagecount/count)
+        
+                message.reply(`Warning stats for this user: ${average}`)
+            }
+        }
+    }
+}
+
+
+
