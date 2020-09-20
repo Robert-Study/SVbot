9e31b5f5df12fc3898250345c6641d25a36b433f
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-20 12:01:15 +0200

    Version 5.4.6

--- features/dynamic/14-flagwords.js
+++ features/dynamic/14-flagwords.js
@@ -1,8 +1,11 @@
 module.exports = (client) => {
 
     client.on("message", async (message) => {
+      return;
         const { flaggedwords } = require('@JSON/flaggedwords.json');
         const { member, content, guild } = message
+        const mention = message.author
+        const logchannel = message.channels.cache.get('730029372697870347')
 
         if (message.author.bot) return;
         if (message.channel.id === "746831486451187753") return;
@@ -12,6 +15,7 @@ module.exports = (client) => {
               message.delete()
               message.reply('your message was flagged for inappropriate content. A moderator will be here soon.')
               message.channel.send(`<@&707581746937462794> __Flagged message:__\n||${content}||`)
+              logchannel.send(`An innapropriate message was detected written by ${mention}`)
               break;
             }
           }
