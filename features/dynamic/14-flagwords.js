a220a30f0f3c8deb8a0171280ea2ddb071ed383b
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-20 12:15:22 +0200

    Version 5.4.6

--- features/dynamic/14-flagwords.js
+++ features/dynamic/14-flagwords.js
@@ -1,28 +1,23 @@
 module.exports = (client) => {
 
-    client.on("message", async (message) => {
-      return;
-        const { flaggedwords } = require('@JSON/flaggedwords.json');
-        const { member, content, guild } = message
-        const mention = message.author
-        const logchannel = message.channels.cache.get('730029372697870347')
+  client.on("message", async (message) => {
+    
+    const { flaggedwords } = require('@JSON/flaggedwords.json');
+    const { member, content, guild } = message
+    const mention = message.author
+    const logchannel = message.channels.cache.get('730029372697870347')
 
-        if (message.author.bot) return;
-        if (message.channel.id === "746831486451187753") return;
+    if (message.author.bot) return;
+    if (message.channel.id === "746831486451187753") return;
 
-        for (var i = 0; i < flaggedwords.length; i++) {
-            if (message.content.includes(flaggedwords[i])) {
-              message.delete()
-              message.reply('your message was flagged for inappropriate content. A moderator will be here soon.')
-              message.channel.send(`<@&707581746937462794> __Flagged message:__\n||${content}||`)
-              logchannel.send(`An innapropriate message was detected written by ${mention}`)
-              break;
-            }
-          }
-
-
-
-
-
-    })
+    for (var i = 0; i < flaggedwords.length; i++) {
+      if (message.content.includes(flaggedwords[i])) {
+        message.delete()
+        message.reply('your message was flagged for inappropriate content. A moderator will be here soon.')
+        message.channel.send(`<@&707581746937462794> __Flagged message:__\n||${content}||`)
+        logchannel.send(`An innapropriate message was detected written by ${mention}`)
+        break;
+      }
+    }
+  })
 }
