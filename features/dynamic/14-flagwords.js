1ab5b5d2f3545bb366fda8d0a90f9425b44ac1b1
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-20 12:30:00 +0200

    Version 5.4.6

--- features/dynamic/14-flagwords.js
+++ features/dynamic/14-flagwords.js
@@ -8,14 +8,15 @@ module.exports = (client) => {
 
     if (message.author.bot) return;
     if (message.channel.id === "746831486451187753") return;
-    
+
     for (var i = 0; i < flaggedwords.length; i++) {
       if (message.content.includes(flaggedwords[i])) {
         message.delete()
         message.reply('your message was flagged for inappropriate content. A moderator will be here soon.')
         message.channel.send(`<@&707581746937462794> __Flagged message:__\n||${content}||`)
+        
       }
-      break;
+      
     }
   })
 }
