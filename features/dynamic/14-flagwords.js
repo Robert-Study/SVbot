bc0ece96adbc7769568d880f53f761ebc7cf8301
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-21 12:01:47 +0200

    Version 5.4.6

--- features/dynamic/14-flagwords.js
+++ features/dynamic/14-flagwords.js
@@ -20,124 +20,129 @@ module.exports = (client) => {
         message.channel.send(` __Flagged message:__\n||${content}||`)
 
         const results = await warningcountSchema
-            .findOneAndUpdate(
-              {
-                UserID: mention,
+          .findOneAndUpdate(
+            {
+              UserID: mention,
+            },
+            {
+              $inc: {
+                warnings: 1,
               },
-              {
-                $inc: {
-                  warnings: 1,
-                },
-              },
-              {
-                upsert: true,
-                new: true,
-              }
-            )
-            .exec()
+            },
+            {
+              upsert: true,
+              new: true,
+            }
+          )
+          .exec()
         console.log(results)
         let warningcount = await warningcountSchema.findOne({
           UserID: author
         })
         console.log(warningcount)
-        
-        let count = warningcount.warnings
-        console.log(count)
 
-        let messages = await messageCountSchema.findOne({
-          UserId: author
-        })
-
-        console.log(messages)
+        for (items of warningcount) {
+          let
+          count = items.warnings
 
 
-        let messagecount = messages.messageCount
-        console.log(messagecount)
+          console.log(count)
 
-        let average = (messagecount / count)
-        console.log(average)
+          let messages = await messageCountSchema.findOne({
+            UserId: author
+          })
 
-        
+          console.log(messages)
 
-        if ((messagecount / count) < 200) {
 
-          guild.members.cache.get(UserId).roles.remove("707547622591692911")
+          let messagecount = messages.messageCount
+          console.log(messagecount)
 
-          const categoryId = "703937876634894387";
-          let role = message.guild.roles.cache.find(role => role.name === "@everyone");
-          let modrole = message.guild.roles.cache.find(role => role.name === "StudyVibes Team");
-          var userName = message.author.username;
-          var userDiscriminator = message.author.discriminator;
+          let average = (messagecount / count)
+          console.log(average)
 
-          var bool = false;
 
-          message.guild.channels.cache.forEach((channel) => {
-            if (channel.name == userName.toLowerCase() + "-" + userDiscriminator) {
 
-              bool = true;
-            }
-          });
+          if ((messagecount / count) < 200) {
 
-          if (bool == true) return;
+            guild.members.cache.get(UserId).roles.remove("707547622591692911")
 
-          message.guild.channels.create(userName + "-" + userDiscriminator, "text").then((createdChan) => {
-            createdChan.setParent(categoryId).then((settedParent) => {
-              settedParent.updateOverwrite(role, {
-                "VIEW_CHANNEL": false,
-                "READ_MESSAGES": false, "SEND_MESSAGES": false,
-                "ATTACH_FILES": false, "CONNECT": false,
-                "CREATE_INSTANT_INVITE": false, "ADD_REACTIONS": false
+            const categoryId = "703937876634894387";
+            let role = message.guild.roles.cache.find(role => role.name === "@everyone");
+            let modrole = message.guild.roles.cache.find(role => role.name === "StudyVibes Team");
+            var userName = message.author.username;
+            var userDiscriminator = message.author.discriminator;
+
+            var bool = false;
+
+            message.guild.channels.cache.forEach((channel) => {
+              if (channel.name == userName.toLowerCase() + "-" + userDiscriminator) {
+
+                bool = true;
+              }
+            });
+
+            if (bool == true) return;
+
+            message.guild.channels.create(userName + "-" + userDiscriminator, "text").then((createdChan) => {
+              createdChan.setParent(categoryId).then((settedParent) => {
+                settedParent.updateOverwrite(role, {
+                  "VIEW_CHANNEL": false,
+                  "READ_MESSAGES": false, "SEND_MESSAGES": false,
+                  "ATTACH_FILES": false, "CONNECT": false,
+                  "CREATE_INSTANT_INVITE": false, "ADD_REACTIONS": false
+                });
+
+                settedParent.updateOverwrite(modrole, {
+                  "VIEW_CHANNEL": true,
+                  "READ_MESSAGES": true, "SEND_MESSAGES": true,
+                  "ATTACH_FILES": true, "CONNECT": true,
+                  "CREATE_INSTANT_INVITE": true, "ADD_REACTIONS": true
+                });
+
+                settedParent.updateOverwrite(message.author, {
+                  "VIEW_CHANNEL": true,
+                  "READ_MESSAGES": true, "SEND_MESSAGES": true,
+                  "ATTACH_FILES": true, "CONNECT": true,
+                  "CREATE_INSTANT_INVITE": false, "ADD_REACTIONS": true
+                });
+
+                var embedParent = new discord.MessageEmbed()
+                  .setTitle("Hi, " + message.author.username.toString())
+                  .setDescription(`You have been quarantained for security reasons. A mod will be here shortly`);
+
+                settedParent.send(embedParent);
+              }).catch(err => {
+                message.channel.send("Something went wrong");
               });
 
-              settedParent.updateOverwrite(modrole, {
-                "VIEW_CHANNEL": true,
-                "READ_MESSAGES": true, "SEND_MESSAGES": true,
-                "ATTACH_FILES": true, "CONNECT": true,
-                "CREATE_INSTANT_INVITE": true, "ADD_REACTIONS": true
-              });
-
-              settedParent.updateOverwrite(message.author, {
-                "VIEW_CHANNEL": true,
-                "READ_MESSAGES": true, "SEND_MESSAGES": true,
-                "ATTACH_FILES": true, "CONNECT": true,
-                "CREATE_INSTANT_INVITE": false, "ADD_REACTIONS": true
-              });
-
-              var embedParent = new discord.MessageEmbed()
-                .setTitle("Hi, " + message.author.username.toString())
-                .setDescription(`You have been quarantained for security reasons. A mod will be here shortly`);
-
-              settedParent.send(embedParent);
             }).catch(err => {
               message.channel.send("Something went wrong");
             });
 
-          }).catch(err => {
-            message.channel.send("Something went wrong");
-          });
 
-          
 
-          break;
+            break;
 
-        }else{
-          const results = await warningcountSchema
-            .findOneAndUpdate(
-              {
-                UserID: mention,
-              },
-              {
-                $inc: {
-                  warnings: 1,
+          } else {
+            const results = await warningcountSchema
+              .findOneAndUpdate(
+                {
+                  UserID: mention,
                 },
-              },
-              {
-                upsert: true,
-              }
-            )
-            .exec()
+                {
+                  $inc: {
+                    warnings: 1,
+                  },
+                },
+                {
+                  upsert: true,
+                }
+              )
+              .exec()
 
-          break;
+            break;
+          }
         }
       }
     }
