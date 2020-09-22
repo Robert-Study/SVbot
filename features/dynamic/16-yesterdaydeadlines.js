42ee2d2991cae85cef1cfb4eb6f72b1d8bb243d2
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-22 08:06:54 +0200

    Version 5.4.6

---
+++ features/dynamic/16-yesterdaydeadlines.js
@@ -0,0 +1,30 @@
+module.exports = (client) => {
+    client.on('ready', async () => {
+
+        var schedule = require('node-schedule');
+
+        var j = schedule.scheduleJob('8 * * * *', async function () {
+            const Deadlineschema = require("@schemas/3-deadlineschema")
+            var moment = require('moment');
+            
+
+            let yesterday = moment().subtract(1, 'days')
+            let yesterdayformat = moment(yesterday).format("DD/MM/YYYY")
+
+            let yesterdayresults = await Deadlineschema.find({
+                date: yesterdayformat
+            })
+            console.log(yesterdayresults)
+
+            if (yesterdayresults) {
+                for (results of yesterdayresults) {
+                    let oldUserID = results.UserID
+
+                    console.log(oldUserID)
+
+                    guild.members.cache.get(oldUserID).roles.remove("755924266779672596")
+                }
+            }
+        })
+    })
+}
