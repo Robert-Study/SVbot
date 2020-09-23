6b81248945c6dbffef709f1f1a29f1a8fbb96dd1
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-23 18:05:14 +0200

    Version 6.4.3

--- features/dynamic/2-forest-and-logs/1-daily-log.js
+++ features/dynamic/2-forest-and-logs/1-daily-log.js
@@ -11,7 +11,7 @@ module.exports = (client) => {
 
         const GuildID = "703937875720273972"
 
-        var j = schedule.scheduleJob('0 * * * *', async function () {
+        var j = schedule.scheduleJob('0 2 * * *', async function () {
 
 
             let newresult = await todaycountschema.updateMany(
@@ -28,5 +28,45 @@ module.exports = (client) => {
             )
             console.log(newresult)
         })
+
+        var K = schedule.scheduleJob('7 * * * *', async function () {
+
+
+            let newresult = await todaycountschema.updateMany(
+                {
+                    GuildID: GuildID
+                },
+                {
+                    $set:
+                    {
+                        daily: 0,
+                        weekly: 0,
+
+                    }
+                }
+            )
+            console.log(newresult)
+        })
+
+        var L = schedule.scheduleJob('8 * * * *', async function () {
+
+
+            let newresult = await todaycountschema.updateMany(
+                {
+                    GuildID: GuildID
+                },
+                {
+                    $set:
+                    {
+                        daily: 0,
+                        weekly: 0,
+                        monthly: 0,
+
+                    }
+                }
+            )
+            console.log(newresult)
+        })
+
     })
 }
