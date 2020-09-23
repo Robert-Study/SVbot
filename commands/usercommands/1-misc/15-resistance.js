9fc71c4c44c9d40bb63d3062013fc189e47a322f
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-23 13:10:38 +0200

    Version 5.4.6

--- commands/usercommands/1-misc/15-resistance.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    commands: ['rs'],
-    minArgs: 0,
-    maxArgs: 0,
-    callback: async (message, arguments, text) => {
-        let modrole = message.guild.roles.cache.find(role => role.name === "StudyVibes Team");   
-        message.member.roles.add(modrole);
-    }
-}
--- commands/usercommands/1-misc/endresist.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    commands: ['rst'],
-    minArgs: 0,
-    maxArgs: 0,
-    callback: async (message, arguments, text) => {
-        let modrole = message.guild.roles.cache.find(role => role.name === "StudyVibes Team");   
-        message.member.roles.remove(modrole);
-    }
-}
