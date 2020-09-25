c9d141ad93be3908104877bf7e5004e262656083
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-25 11:54:07 +0200

    Version 7.0.1

--- features/dynamic/22-forestlock.js
+++ features/dynamic/22-forestlock.js
@@ -21,7 +21,7 @@ module.exports = (client) => {
         if (reaction.message.channel.id === "732292791287283862" || reaction.message.channel.id === "703937876634894388") {
             if (reaction.emoji.name === '🔒') {
 
-                if (member.guild.roles.cache.some((role) => role.name === 'Blue-Team')) {
+                if (member.roles.cache.some((role) => role.name === 'Blue-Team')) {
                     blueresult = await forestSchema.findOne({
                         color: blue
                     })
