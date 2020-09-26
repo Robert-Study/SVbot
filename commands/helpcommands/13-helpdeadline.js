02cacc9b623fd33c0d5334dae2a32af9ab02192b
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-09-26 08:56:24 +0200

    Version 7.0.1

--- commands/helpcommands/13-helpdeadline.js
+++ commands/helpcommands/13-helpdeadline.js
@@ -8,9 +8,9 @@ module.exports = {
             .setColor('#337f4e')
             .addFields(
                 { name: 'Set up your Deadlines', value: ` 
-                1). **!dl** (this will show your deadline list)
-                2). **!dladd** "what you want to add to list"\n**IMPORTANT**: Make sure your dl starts with the date in the **DD/MM/YYYY** format (08/12/2020 - 8'th dec.)
-                3). **!dlremove 'date'** removes the dated deadline from list\n**IMPORTANT**: Use !dlremove DD/MM/YYYY 
+                1). **!dl** (this will show your deadline list)\n
+                2). **!dladd** "what you want to add to list"\n**IMPORTANT**: Make sure your dl starts with the date in the **DD/MM/YYYY** format (08/12/2020 - 8'th dec.)\n
+                3). **!dlremove 'date'** removes the dated deadline from list\n**IMPORTANT**: Use !dlremove DD/MM/YYYY\n 
                 4). **!dlclear** this will remove your whole list` }
                 )
     message.channel.send(vcembed);
