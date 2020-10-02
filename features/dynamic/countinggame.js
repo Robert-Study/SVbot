c9eb1cd0c59f0a14140148566aea6a933d043915
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-10-02 16:19:18 +0200

    Version 7.3.0

--- features/dynamic/countinggame.js
+++ features/dynamic/countinggame.js
@@ -3,7 +3,7 @@ const { Guild } = require('discord.js');
 module.exports = (client) => {
 
     client.on("message", async (message) => {
-        
+
         const messageCountSchema = require('@schemas/19-countschema')
 
         if (message.author.bot) return;
@@ -20,170 +20,170 @@ module.exports = (client) => {
             })
 
             if (count) {
-                for (items of count) {
-                    let currentcount = items.number
 
-                    let usercount = message.content
-                    let lastcounter = items.lastuser
-                    let addone = (currentcount + 1)
+                let currentcount = number
 
-                    if (isNaN(usercount) || usercount != (addone) || User === lastcounter) {
-                        let failure = await messageCountSchema.findOne({
-                            GuildID: Guild,
-                            UserID: 'countgame'
-                        })
+                let usercount = message.content
+                let lastcounter = lastuser
+                let addone = (currentcount + 1)
 
-                        for (resultaten of failure) {
-                            let savegame = resultaten.gameresc
+                if (isNaN(usercount) || usercount != (addone) || User === lastcounter) {
+                    let failure = await messageCountSchema.findOne({
+                        GuildID: Guild,
+                        UserID: 'countgame'
+                    })
 
-                            if (savegame === 0) {
-                                message.reply("That was the wrong number, there are no saves left. Start again at 1!")
-                                let failure = await messageCountSchema.findOneAndUpdate(
-                                    {
-                                        UserID: 'countgame',
+                    for (resultaten of failure) {
+                        let savegame = resultaten.gameresc
+
+                        if (savegame === 0) {
+                            message.reply("That was the wrong number, there are no saves left. Start again at 1!")
+                            let failure = await messageCountSchema.findOneAndUpdate(
+                                {
+                                    UserID: 'countgame',
+                                },
+                                {
+                                    UserID: 'countgame',
+                                    number: 0,
+                                    lastuser: 'reset'
+                                })
+
+
+                            let userfailure = await messageCountSchema.findOneAndUpdate(
+                                {
+                                    UserID: User,
+                                },
+                                {
+                                    UserID: User,
+                                    $inc: {
+                                        wrong: 1,
                                     },
-                                    {
-                                        UserID: 'countgame',
-                                        number: 0,
-                                        lastuser: 'reset'
-                                    })
-
-
-                                let userfailure = await messageCountSchema.findOneAndUpdate(
-                                    {
-                                        UserID: User,
+                                },
+                                {
+                                    upsert: true,
+                                    new: true,
+                                })
+                        } if (savegame > 0) {
+                            message.reply(`That was the wrong number, luckily this server has a save. Count on with the number **${addone}**`)
+                            let savedgame = await messageCountSchema.findOneAndUpdate(
+                                {
+                                    UserID: 'countgame',
+                                },
+                                {
+                                    UserID: 'countgame',
+                                    $inc: {
+                                        gameresc: -1,
                                     },
-                                    {
-                                        UserID: User,
-                                        $inc: {
-                                            wrong: 1,
-                                        },
-                                    },
-                                    {
-                                        upsert: true,
-                                        new: true,
-                                    })
-                            } if (savegame > 0) {
-                                message.reply(`That was the wrong number, luckily this server has a save. Count on with the number **${addone}**`)
-                                let savedgame = await messageCountSchema.findOneAndUpdate(
-                                    {
-                                        UserID: 'countgame',
-                                    },
-                                    {
-                                        UserID: 'countgame',
-                                        $inc: {
-                                            gameresc: -1,
-                                        },
-                                    },
-                                    {
-                                        upsert: true,
-                                        new: true,
-                                    })
-
-
-                            }
+                                },
+                                {
+                                    upsert: true,
+                                    new: true,
+                                })
 
 
                         }
-                    }
-                    else {
-                        let newcount = await messageCountSchema.findOneAndUpdate(
-                            {
-                                UserID: 'countgame',
-                            },
-                            {
-                                UserID: 'countgame',
-                                number: addone,
-                                lastuser: User
-                            })
 
-                        let userscore = await messageCountSchema.findOneAndUpdate(
+
+                    }
+                }
+                else {
+                    let newcount = await messageCountSchema.findOneAndUpdate(
+                        {
+                            UserID: 'countgame',
+                        },
+                        {
+                            UserID: 'countgame',
+                            number: addone,
+                            lastuser: User
+                        })
+
+                    let userscore = await messageCountSchema.findOneAndUpdate(
+                        {
+                            UserID: User,
+                        },
+                        {
+                            UserID: User,
+                            $inc: {
+                                number: 1,
+                            },
+                        },
+                        {
+                            upsert: true,
+                            new: true,
+                        })
+
+                    if (addone = 100) {
+                        gamechannel.send(`GG <@&${User}> you have counted to 100 and earned a save for the server!`)
+                        let newsave = await messageCountSchema.findOneAndUpdate(
                             {
-                                UserID: User,
+                                UserID: 'countgame'
                             },
                             {
-                                UserID: User,
+                                UserID: 'countgame',
                                 $inc: {
-                                    number: 1,
+                                    gameresc: 1,
                                 },
                             },
                             {
                                 upsert: true,
                                 new: true,
                             })
+                    }
 
-                        if (addone = 100) {
-                            gamechannel.send(`GG <@&${User}> you have counted to 100 and earned a save for the server!`)
-                            let newsave = await messageCountSchema.findOneAndUpdate(
-                                {
-                                    UserID: 'countgame'
+                    if (addone = 500) {
+                        gamechannel.send(`GG <@&${User}> you have counted to 500 and earned a save for the server!`)
+                        let newsave = await messageCountSchema.findOneAndUpdate(
+                            {
+                                UserID: 'countgame'
+                            },
+                            {
+                                UserID: 'countgame',
+                                $inc: {
+                                    gameresc: 1,
                                 },
-                                {
-                                    UserID: 'countgame',
-                                    $inc: {
-                                        gameresc: 1,
-                                    },
-                                },
-                                {
-                                    upsert: true,
-                                    new: true,
-                                })
-                        }
+                            },
+                            {
+                                upsert: true,
+                                new: true,
+                            })
+                    }
 
-                        if (addone = 500) {
-                            gamechannel.send(`GG <@&${User}> you have counted to 500 and earned a save for the server!`)
-                            let newsave = await messageCountSchema.findOneAndUpdate(
-                                {
-                                    UserID: 'countgame'
+                    if (addone = 1000) {
+                        gamechannel.send(`GG <@&${User}> you have counted to 1000 and earned a save for the server!`)
+                        let newsave = await messageCountSchema.findOneAndUpdate(
+                            {
+                                UserID: 'countgame'
+                            },
+                            {
+                                UserID: 'countgame',
+                                $inc: {
+                                    gameresc: 1,
                                 },
-                                {
-                                    UserID: 'countgame',
-                                    $inc: {
-                                        gameresc: 1,
-                                    },
-                                },
-                                {
-                                    upsert: true,
-                                    new: true,
-                                })
-                        }
+                            },
+                            {
+                                upsert: true,
+                                new: true,
+                            })
+                    }
 
-                        if (addone = 1000) {
-                            gamechannel.send(`GG <@&${User}> you have counted to 1000 and earned a save for the server!`)
-                            let newsave = await messageCountSchema.findOneAndUpdate(
-                                {
-                                    UserID: 'countgame'
-                                },
-                                {
-                                    UserID: 'countgame',
-                                    $inc: {
-                                        gameresc: 1,
-                                    },
-                                },
-                                {
-                                    upsert: true,
-                                    new: true,
-                                })
-                        }
+                    let highscore = await messageCountSchema.findOne({
+                        UserID: 'highscore'
+                    })
 
-                        let highscore = await messageCountSchema.findOne({
-                            UserID: 'highscore'
-                        })
-
-                        for (results of highscore) {
-                            let highscore = results.number
-                            if (highscore <= addone) {
-                                let newhighscore = await messageCountSchema.findOneAndUpdate(
-                                    {
-                                        UserID: 'highscore'
-                                    }, {
-                                    UserID: 'highscore',
-                                    number: addone
-                                })
-                            }
+                    for (results of highscore) {
+                        let highscore = results.number
+                        if (highscore <= addone) {
+                            let newhighscore = await messageCountSchema.findOneAndUpdate(
+                                {
+                                    UserID: 'highscore'
+                                }, {
+                                UserID: 'highscore',
+                                number: addone
+                            })
                         }
                     }
                 }
+
             }
         }
     })
