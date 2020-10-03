ed988c4799b50efc6d40d9c100b24b92cd54a320
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-10-03 10:51:23 +0200

    Version 7.5.0

--- features/dynamic/19-simonsays.js
+++ features/dynamic/19-simonsays.js
@@ -22,7 +22,7 @@ module.exports = (client) => {
 
                 if (current === simontime) {
                     const Discord = require('discord.js')
-                    const collector = new Discord.MessageCollector(filter, {time: 30000, errors: ['time'] });
+                    const collector = new Discord.MessageCollector(filter, { time: 30000, errors: ['time'] });
                     const { simonWords } = require('@JSON/simonWords.json');
                     randomword = simonWords[Math.floor(Math.random() * simonWords.length)]
 
@@ -33,7 +33,7 @@ module.exports = (client) => {
                             const mention = message.author
                             const user = mention.id
                             if (userresponse === randomword) {
-
+                                console.log(good)
                                 let B = await simonSchema.findOneAndUpdate(
                                     {
                                         GuildID: Guild,
@@ -77,7 +77,53 @@ module.exports = (client) => {
                                     let userscore = C.number
                                     let userwrong = C.wrong
                                     message.reply(`that was the correct response, adding one point.\nYour total score is now ${userscore} correct, ${userwrong} wrong responses`)
-                                } else { gamechannel.send("Something went wrong, error 12") }
+                                    let randomize = Math.floor(Math.random() * 50) + 1;
+                                    let randomminute = `${randomize}m`
+                                    let addthis = ms(`${randomminute}`)
+
+                                    let startTime = new Date(Date.now());
+                                    let endTime = new Date(startTime.getTime() + addthis);
+                                    let structuretime = moment(endTime).format('DD/MM/YYYY-hh:mm')
+
+                                    let F = await simonSchema.findOneAndUpdate(
+                                        {
+                                            GuildID: server,
+                                            UserID: 'gameset',
+                                        },
+                                        {
+                                            UserID: 'gameset',
+                                            time: structuretime
+
+                                        },
+                                        {
+                                            upsert: true,
+                                            new: true,
+                                        })
+                                } else {
+                                    gamechannel.send("Something went wrong, error 12")
+                                    let randomize = Math.floor(Math.random() * 50) + 1;
+                                    let randomminute = `${randomize}m`
+                                    let addthis = ms(`${randomminute}`)
+
+                                    let startTime = new Date(Date.now());
+                                    let endTime = new Date(startTime.getTime() + addthis);
+                                    let structuretime = moment(endTime).format('DD/MM/YYYY-hh:mm')
+
+                                    let F = await simonSchema.findOneAndUpdate(
+                                        {
+                                            GuildID: server,
+                                            UserID: 'gameset',
+                                        },
+                                        {
+                                            UserID: 'gameset',
+                                            time: structuretime
+
+                                        },
+                                        {
+                                            upsert: true,
+                                            new: true,
+                                        })
+                                }
 
                             } if (userresponse != randomword) {
                                 let D = await simonSchema.findOneAndUpdate(
@@ -123,34 +169,83 @@ module.exports = (client) => {
                                     let userscore = E.number
                                     let userwrong = E.wrong
                                     message.reply(`You got it wrong this time!.\nYour total score is now ${userscore} correct, ${userwrong} wrong responses`)
-                                } else { gamechannel.send("Something went wrong, error 13") }
+                                    let randomize = Math.floor(Math.random() * 50) + 1;
+                                    let randomminute = `${randomize}m`
+                                    let addthis = ms(`${randomminute}`)
+
+                                    let startTime = new Date(Date.now());
+                                    let endTime = new Date(startTime.getTime() + addthis);
+                                    let structuretime = moment(endTime).format('DD/MM/YYYY-hh:mm')
+
+                                    let F = await simonSchema.findOneAndUpdate(
+                                        {
+                                            GuildID: server,
+                                            UserID: 'gameset',
+                                        },
+                                        {
+                                            UserID: 'gameset',
+                                            time: structuretime
+
+                                        },
+                                        {
+                                            upsert: true,
+                                            new: true,
+                                        })
+                                } else {
+                                    gamechannel.send("Something went wrong, error 13")
+                                    let randomize = Math.floor(Math.random() * 50) + 1;
+                                    let randomminute = `${randomize}m`
+                                    let addthis = ms(`${randomminute}`)
+
+                                    let startTime = new Date(Date.now());
+                                    let endTime = new Date(startTime.getTime() + addthis);
+                                    let structuretime = moment(endTime).format('DD/MM/YYYY-hh:mm')
+
+                                    let F = await simonSchema.findOneAndUpdate(
+                                        {
+                                            GuildID: server,
+                                            UserID: 'gameset',
+                                        },
+                                        {
+                                            UserID: 'gameset',
+                                            time: structuretime
+
+                                        },
+                                        {
+                                            upsert: true,
+                                            new: true,
+                                        })
+                                }
 
                             }
-                        } catch { gamechannel.send('No one responded to me in time. Wait for the next word.') }
+                        } catch {
+                            gamechannel.send('No one responded to me in time. Wait for the next word.') 
+                            let randomize = Math.floor(Math.random() * 50) + 1;
+                            let randomminute = `${randomize}m`
+                            let addthis = ms(`${randomminute}`)
+
+                            let startTime = new Date(Date.now());
+                            let endTime = new Date(startTime.getTime() + addthis);
+                            let structuretime = moment(endTime).format('DD/MM/YYYY-hh:mm')
+
+                            let F = await simonSchema.findOneAndUpdate(
+                                {
+                                    GuildID: server,
+                                    UserID: 'gameset',
+                                },
+                                {
+                                    UserID: 'gameset',
+                                    time: structuretime
+
+                                },
+                                {
+                                    upsert: true,
+                                    new: true,
+                                })
+                        }
                     })
 
-                } let randomize = Math.floor(Math.random() * 50) + 1;
-                let randomminute = `${randomize}m`
-                let addthis = ms(`${randomminute}`)
-
-                let startTime = new Date(Date.now());
-                let endTime = new Date(startTime.getTime() + addthis);
-                let structuretime = moment(endTime).format('DD/MM/YYYY-hh:mm')
-
-                let F = await simonSchema.findOneAndUpdate(
-                    {
-                        GuildID: server,
-                        UserID: 'gameset',
-                    },
-                    {
-                        UserID: 'gameset',
-                        time: structuretime
-
-                    },
-                    {
-                        upsert: true,
-                        new: true,
-                    })
+                }
             }
         })
     })
