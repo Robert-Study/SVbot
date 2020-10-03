3efc1a6ca2ed5fc7fbecdb54d4fc4e13fd7e89fb
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-10-03 15:04:49 +0200

    Version 7.5.1

---
+++ commands/usercommands/1-misc/20-history.js
@@ -0,0 +1,27 @@
+module.exports = {
+    commands: ['history'],
+    minArgs: 0,
+    maxArgs: 0,
+    callback: (message, arguments, text) => {
+        const Discord = require("discord.js");
+        const snekfetch = require('snekfetch');
+
+        const { text } = await snekfetch
+            .get('http://history.muffinlabs.com/date');
+        const body = JSON.parse(text);
+        const events = body.data.Events;
+        const event = events[Math.floor(Math.random() * events.length)];
+        const embed = new Discord.MessageEmbed()
+            .setColor(0x00A2E8)
+            .setURL(body.url)
+            .setTitle(`On this day (${body.date})...`)
+            .setTimestamp()
+            .setDescription(`${event.year}: ${event.text}`);
+        return message.channel.send(embed)
+    }
+}
+
+
+
+
+
