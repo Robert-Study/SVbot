a6fef5e1d14ebd894e92a497ad3545aafc4ce1a8
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-10-04 00:49:42 +0200

    Version 7.6.3

--- commands/usercommands/1-misc/20-history.js
+++ commands/usercommands/1-misc/20-history.js
@@ -7,20 +7,18 @@ module.exports = {
         const snekfetch = require('snekfetch');
         const fetch = require('node-fetch');
 
-        const { website } = await fetch('http://history.muffinlabs.com/date').then(() => {
-            
-            const body = JSON.parse(website);
-            const events = body.data.Events;
-            const event = events[Math.floor(Math.random() * events.length)];
-            const embed = new Discord.MessageEmbed()
-                .setColor(0x00A2E8)
-                .setURL(body.url)
-                .setTitle(`On this day (${body.date})...`)
-                .setTimestamp()
-                .setDescription(`${event.year}: ${event.text}`);
-
-            return message.channel.send(embed)
-        })
+        const { text } = await snekfetch
+            .get('http://history.muffinlabs.com/date');
+        const body = JSON.parse(text);
+        const events = body.data.Events;
+        const event = events[Math.floor(Math.random() * events.length)];
+        const embed = new Discord.MessageEmbed()
+            .setColor(0x00A2E8)
+            .setURL(body.url)
+            .setTitle(`On this day (${body.date})...`)
+            .setTimestamp()
+            .setDescription(`${event.year}: ${event.text}`);
+        return message.channel.send(embed)
     }
 }
 
