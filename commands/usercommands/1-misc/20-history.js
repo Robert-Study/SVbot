1af18476d85b68fcffa6966c5907b1ae2c538b5c
RobinJans-SV <68498939+RobinJans-SV@users.noreply.github.com>
2020-10-03 16:24:07 +0200

    Version 7.5.3

--- commands/usercommands/1-misc/20-history.js
+++ commands/usercommands/1-misc/20-history.js
@@ -7,19 +7,20 @@ module.exports = {
         const snekfetch = require('snekfetch');
         const fetch = require('node-fetch');
 
-        const { website } = await fetch('http://history.muffinlabs.com/date');
-        console.log(website)
-        const body = JSON.parse(website);
-        const events = body.data.Events;
-        const event = events[Math.floor(Math.random() * events.length)];
-        const embed = new Discord.MessageEmbed()
-            .setColor(0x00A2E8)
-            .setURL(body.url)
-            .setTitle(`On this day (${body.date})...`)
-            .setTimestamp()
-            .setDescription(`${event.year}: ${event.text}`);
+        const { website } = await fetch('http://history.muffinlabs.com/date').then(() => {
+            console.log(website)
+            const body = JSON.parse(website);
+            const events = body.data.Events;
+            const event = events[Math.floor(Math.random() * events.length)];
+            const embed = new Discord.MessageEmbed()
+                .setColor(0x00A2E8)
+                .setURL(body.url)
+                .setTitle(`On this day (${body.date})...`)
+                .setTimestamp()
+                .setDescription(`${event.year}: ${event.text}`);
 
-        return message.channel.send(embed)
+            return message.channel.send(embed)
+        })
     }
 }
 
